d3f5cc8830233d7bccafa6e22bfba15fdb1bad6a
Stefan Hendricks <stefan.hendricks@awi.de>
2015-07-09 17:12:59 +0200

    [pysiral] Added class for CryoSat-2 L1b DSD's

--- pysiral/l1bfile.py
+++ pysiral/l1bfile.py
@@ -267,6 +267,84 @@ class CS2L1bSpecificProductHeader(CS2L1bBaseHeader):
         return re.search("L1B_PROC_THRESH=", line)
 
 
+class CS2L1bScienceDataSetDescriptors(object):
+    """
+    Container for the Data Set Descriptors (dsd's) in the L1B file
+    For each dsd there is an attribute with its name (dsd_name)
+    that stores the information as a dictionary
+    """
+
+    _DSD_REFDICT = {
+        "ds_type": "",
+        "filename": "",
+        "ds_offset": 0,
+        "ds_size": 0,
+        "num_dsr": 0,
+        "dsr_size": 0
+    }
+
+    _DSD_UNITS = {
+        "ds_type": None,
+        "filename": None,
+        "ds_offset": "bytes",
+        "ds_size": "bytes",
+        "num_dsr": None,
+        "dsr_size": "bytes"
+    }
+
+    # There are 8 lines to read for each DSD records
+    # 7 parameters + 1 empty line
+    _N_RECORDS_PER_DSD = 8
+
+    def __init__(self):
+        self._current_dsd = None
+        self.unit_dict = self._DSD_UNITS
+        self.field_list = []
+
+    def _is_dsd_start(self, tag):
+        """ Checks if the current tag name indicated the start of a new dsd """
+        return tag == "ds_name"
+
+    def _append_field(self, tag, value):
+        """ Adds a field to the current data set descriptor """
+        dsd_dict = getattr(self, self._current_dsd)
+        dsd_dict[tag] = value
+        setattr(self, self._current_dsd, dsd_dict)
+
+    def get_num_lines(self, n_dsd):
+        return n_dsd*self._N_RECORDS_PER_DSD
+
+    def parse_line(self, line):
+        """
+        Parses a given line in the dsd block and addes the content
+        to the dsd object
+        """
+        # Parse the line
+        tag, value, unit = parse_cryosat2_l1b_header_field(line)
+        # Skip empty line
+        if tag is None:
+            return
+        # Check if start of new dsd and create a new one
+        if self._is_dsd_start(tag):
+            setattr(self, value, self._DSD_REFDICT.copy())
+            self._current_dsd = value
+            self.field_list.append(value)
+            return
+        # save the field
+        self._append_field(tag, value)
+
+    def __repr__(self):
+        """ String representation of the dsd's """
+        output = ""
+        for field in self.field_list:
+            output += "DSD: %s\n" % (field)
+            dsd_dict = getattr(self, field)
+            for key in dsd_dict.keys():
+                output += "  %s: %s, unit: %s\n" % (
+                    key, str(dsd_dict[key]), str(self.unit_dict[key]))
+        return output
+
+
 def parse_cryosat_l1b_filename(filename):
     """
     Returns the information in the CryoSat-2 l1b filename
