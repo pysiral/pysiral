b9ade5d8fd5d567331a91d54df1b58ce49eb8d70
Stefan Hendricks <stefan.hendricks@awi.de>
2015-07-13 15:18:00 +0200

    [pysiral] Switched msd definitions to construct type nested structures

--- pysiral/cryosat2/l1b_msd_def.py
+++ pysiral/cryosat2/l1b_msd_def.py
@@ -5,136 +5,203 @@ Created on Fri Jul 10 21:00:37 2015
 @author=Stefan
 """
 
-from collections import namedtuple
-import struct
+from construct import (Struct, Array, Padding, Adapter, Bit, BitStruct,
+                       SBInt16, UBInt16, SBInt32, UBInt32, SBInt64, UBInt64)
 
 
-class Cryosat2L1bMSDDefinitionSarBaselineC(object):
+class OneHundredth(Adapter):
+    def _decode(self, obj, context):
+        return float(obj)/100
+
+
+class OneHundredthDecibel(Adapter):
+    def _decode(self, obj, context):
+        return float(obj)/100
+
+
+class OneTenthMicroDeg(Adapter):
+    def _decode(self, obj, context):
+        return float(obj)*1e-7
+
+
+class MilliMeter(Adapter):
+    def _decode(self, obj, context):
+        return float(obj)*1e-3
+
+
+class Micrometer(Adapter):
+    def _decode(self, obj, context):
+        return float(obj)*1e-6
+
+
+class PicoSecond(Adapter):
+    def _decode(self, obj, context):
+        return float(obj)*1e-12
+
+
+class MicroWatts(Adapter):
+    def _decode(self, obj, context):
+        return float(obj)*1e-6
+
+
+class MicroRadians(Adapter):
+    def _decode(self, obj, context):
+        return float(obj)*1e-6
+
+
+class OneTenthMicroRadians(Adapter):
+    def _decode(self, obj, context):
+        return float(obj)*1e-6
+
+
+class Cryosat2L1bMSDDefinition(object):
     """ Holds the Definition of L1b MDS records """
 
     def __init__(self):
 
-        # This definition holds the following msd fields:
-        self.msd_groups = [
-            "time_orbit_group",
-            "measurement_group",
-            "geocorrections_group",
-            "onehz_wavefrom_group",
-            "waveform_group"]
+        self.baseline = None
+        self.radar_mode = None
+        self.n_records = 0
 
-        # Default definition of fields
-        mds_def = namedtuple('mds_def', ['name', 'fmt', 'size'])
+    def get_msd_parser(self):
 
-        self.time_orbit_group = [
-            mds_def(name="day", fmt="L", size=4),
-            mds_def(name="sec", fmt="l", size=4),
-            mds_def(name="msec", fmt="l", size=4),
-            mds_def(name="uso_corr", fmt="l", size=4),
-            mds_def(name="mode_id", fmt="H", size=2),
-            mds_def(name="source_sequence_counter", fmt="H", size=2),
-            mds_def(name="instrument_configuration", fmt="L", size=4),
-            mds_def(name="burst_counter", fmt="L", size=4),
-            mds_def(name="measurement_latitude", fmt="l", size=4),
-            mds_def(name="measurement_longtitude", fmt="l", size=4),
-            mds_def(name="altitude_cog", fmt="L", size=4),
-            mds_def(name="altitude_rate", fmt="L", size=4),
-            mds_def(name="satellite_velocity", fmt="(3l)", size=12),
-            mds_def(name="real_beam", fmt="(3L)", size=12),
-            mds_def(name="interferometry_baseline", fmt="(3l)", size=12),
-            mds_def(name="star_tracker_usage", fmt="H", size=2),
-            mds_def(name="antenna_roll", fmt="l", size=4),
-            mds_def(name="antenna_pitch", fmt="l", size=4),
-            mds_def(name="antenna_yaw", fmt="l", size=4),
-            mds_def(name="fbr_confidence_flag", fmt="l", size=4),
-            mds_def(name="spare", fmt="4c", size=4)]
+        self.time_orbit_group = Struct(
+            "time_orbit",
+            SBInt32("day"),
+            UBInt32("sec"),
+            UBInt32("msec"),
+            UBInt16("uso_corr"),
+            UBInt32("mode_id"),
+            UBInt16("source_sequence_counter"),
+            UBInt32("instrument_configuration"),
+            UBInt32("burst_counter"),
+            OneTenthMicroDeg(SBInt32("atitude")),
+            OneTenthMicroDeg(SBInt32("longitude")),
+            MilliMeter(SBInt32("altitude_cog")),
+            MilliMeter(SBInt32("altitude_rate")),
+            Array(3, MilliMeter(SBInt32("satellite_velocity"))),
+            Array(3, Micrometer(SBInt32("real_beam"))),
+            Array(3, Micrometer(SBInt32("interferometry_baseline"))),
+            UBInt16("star_tracker_usage"),
+            OneTenthMicroDeg(SBInt32("antenna_roll")),
+            OneTenthMicroDeg(SBInt32("antenna_pitch")),
+            OneTenthMicroDeg(SBInt32("antenna_yaw")),
+            UBInt32("fbr_confidence_flag"),
+            Padding(4))
 
-        # measurement group
-        self.measurement_group = [
-            mds_def(name="td", fmt="q", size=8),
-            mds_def(name="h0", fmt="l", size=4),
-            mds_def(name="cor2", fmt="l", size=4),
-            mds_def(name="lai", fmt="l", size=4),
-            mds_def(name="fai", fmt="l", size=4),
-            mds_def(name="agc_ch1", fmt="l", size=4),
-            mds_def(name="agc_ch2", fmt="l", size=4),
-            mds_def(name="tr_gain_ch1", fmt="l", size=4),
-            mds_def(name="tr_gain_ch2", fmt="l", size=4),
-            mds_def(name="tx_power", fmt="l", size=4),
-            mds_def(name="dopp_rc", fmt="l", size=4),
-            mds_def(name="tr_inst_rc", fmt="l", size=4),
-            mds_def(name="r_inst_rc", fmt="l", size=4),
-            mds_def(name="tr_inst_gain_c", fmt="l", size=4),
-            mds_def(name="r_inst_gain_c", fmt="l", size=4),
-            mds_def(name="int_phase_c", fmt="l", size=4),
-            mds_def(name="ext_phase_c", fmt="l", size=4),
-            mds_def(name="noise_pwr", fmt="l", size=4),
-            mds_def(name="phase_slope_c", fmt="l", size=4),
-            mds_def(name="spares", fmt="4b", size=4)]
+        self.measurement_group = Struct(
+            "measurement",
+            PicoSecond(SBInt64("window_delay")),
+            SBInt32("h0"),
+            SBInt32("cor2"),
+            SBInt32("lai"),
+            SBInt32("fai"),
+            OneHundredthDecibel(SBInt32("agc_1")),
+            OneHundredthDecibel(SBInt32("agc_2")),
+            OneHundredthDecibel(SBInt32("fixed_gain_1")),
+            OneHundredthDecibel(SBInt32("fixed_gain_2")),
+            MicroWatts(SBInt32("tx_power")),
+            MilliMeter(SBInt32("doppler_range_correction")),
+            MilliMeter(SBInt32("instrument_range_correction_txrx")),
+            MilliMeter(SBInt32("instrument_range_correction_rx")),
+            OneHundredthDecibel(SBInt32("instrument_gain_correction_txrx")),
+            OneHundredthDecibel(SBInt32("instrument_gain_correction_rx")),
+            MicroRadians(SBInt32("phase_correction_internal")),
+            MicroRadians(SBInt32("phase_correction_external")),
+            OneHundredthDecibel(SBInt32("noise_power")),
+            MicroRadians(SBInt32("phase_slope_correction")),
+            Padding(4))
 
-        # Geocorrections group
-        self.geocorrections_group = [
-            mds_def(name="dry_c", fmt="l", size=4),
-            mds_def(name="wet_c", fmt="l", size=4),
-            mds_def(name="ib_c", fmt="l", size=4),
-            mds_def(name="dac_c", fmt="l", size=4),
-            mds_def(name="iono_gim", fmt="l", size=4),
-            mds_def(name="iono_mod", fmt="l", size=4),
-            mds_def(name="h_ot", fmt="l", size=4),
-            mds_def(name="h_lpeot", fmt="l", size=4),
-            mds_def(name="h_olt", fmt="l", size=4),
-            mds_def(name="h_set", fmt="l", size=4),
-            mds_def(name="h_gpt", fmt="l", size=4),
-            mds_def(name="surf_type", fmt="L", size=4),
-            mds_def(name="spare1", fmt="4b", size=4),
-            mds_def(name="corr_status", fmt="L", size=4),
-            mds_def(name="corr_error", fmt="L", size=4),
-            mds_def(name="spare2", fmt="4b", size=4)]
+        self.corrections_group = Struct(
+            "corrections",
+            MilliMeter(SBInt32("dry_troposphere")),
+            MilliMeter(SBInt32("wet_troposphere")),
+            MilliMeter(SBInt32("inverse_barometric")),
+            MilliMeter(SBInt32("dynamic_atmosphere")),
+            MilliMeter(SBInt32("ionosphere_gim")),
+            MilliMeter(SBInt32("ionosphere_mod")),
+            MilliMeter(SBInt32("ocean_tide_elastic")),
+            MilliMeter(SBInt32("ocean_tide_long_period")),
+            MilliMeter(SBInt32("ocean_loading_tide")),
+            MilliMeter(SBInt32("solid_earth_tide")),
+            MilliMeter(SBInt32("geocentric_polar_tide")),
+            UBInt32("surface_type"),
+            Padding(4),
+            UBInt32("correction_status"),
+            UBInt32("correction_error"),
+            Padding(4))
 
-        # 1Hz Average SAR Waveform group
-        self.onehz_waveform_group = [
-            mds_def(name="day_1hz", fmt="l", size=4),
-            mds_def(name="sec_1hz", fmt="L", size=4),
-            mds_def(name="micsec_1hz", fmt="L", size=4),
-            mds_def(name="lat_1hz", fmt="l", size=4),
-            mds_def(name="lon_1hz", fmt="l", size=4),
-            mds_def(name="alt_1hz", fmt="l", size=4),
-            mds_def(name="td_1hz", fmt="q", size=8),
-            mds_def(name="avg_wfm", fmt="512H", size=1024),
-            mds_def(name="linear_wfm_mult", fmt="l", size=4),
-            mds_def(name="power2_wfm_mult", fmt="l", size=4),
-            mds_def(name="num_avg_echoes", fmt="H", size=2),
-            mds_def(name="flags", fmt="H", size=2)]
+        self.onehz_waveform_group = Struct(
+            "avg_waveform",
+            SBInt32("day"),
+            UBInt32("sec"),
+            UBInt32("msec"),
+            OneTenthMicroDeg(SBInt32("latitude")),
+            OneTenthMicroDeg(SBInt32("longitude")),
+            MilliMeter(SBInt32("altitude_cog")),
+            PicoSecond(UBInt64("window_delay")),
+            Array(128, UBInt16("wfm")),
+            SBInt32("echo_scale_watts"),
+            SBInt32("echo_scale_power"),
+            UBInt16("num_avg_echoes"),
+            UBInt16("flags"))
 
-        # 20 Hz SAR Waveform group
-        self.waveform_group = [
-            mds_def(name="wfm", fmt="256H", size=512),
-            mds_def(name="linear_wfm_multiplier", fmt="l", size=4),
-            mds_def(name="power2_wfm_multiplier", fmt="l", size=4),
-            mds_def(name="num_avg_echoes", fmt="H", size=2),
-            mds_def(name="flags", fmt="H", size=2),
-            mds_def(name="beam", fmt="100b", size=100)]
+        self.waveform_flag_group = BitStruct(
+            "flag",
+            Bit("approximate_beam_steering"),
+            Bit("exact_beam_steering"),
+            Bit("doppler_weighting_computed"),
+            Bit("dopple_weighting_applied_before_stack"),
+            Bit("multi_look_incomplete"),
+            Bit("beam_angle_steering_error"),
+            Bit("anti_aliased_power_echoes"),
+            Bit("auto_beam_steering"),
+            Padding(8))
+
+        self.waveform_beam_group = Struct(
+            "beam",
+            OneHundredth(UBInt16("stack_standard_deviation")),
+            OneHundredth(UBInt16("stack_centre")),
+            OneHundredthDecibel(SBInt16("stack_scaled_amplitude")),
+            OneHundredth(SBInt16("stack_skewness")),
+            OneHundredth(SBInt16("stack_kurtosis")),
+            MicroRadians(SBInt16("stack_standard_deviation_boresight")),
+            MicroRadians(SBInt16("stack_centre_angle")),
+            OneTenthMicroRadians(SBInt32("doppler_angle_start")),
+            OneTenthMicroRadians(SBInt32("doppler_angle_stop")),
+            OneTenthMicroRadians(SBInt32("look_angle_start")),
+            OneTenthMicroRadians(SBInt32("look_angle_stop")),
+            UBInt16("n_beams_after_weighing"),
+            UBInt16("n_beams_before_weighing"),
+            Padding(66))
+
+        self.waveform_group = Struct(
+            "waveform",
+            Array(256, UBInt16("wfm")),
+            SBInt32("linear_wfm_multiplier"),
+            SBInt32("power2_wfm_multiplier"),
+            UBInt16("num_avg_echoes"),
+            self.waveform_flag_group,
+            self.waveform_beam_group)
+
+        self.mds_record = Struct(
+            "mds_record",
+            Array(20, self.time_orbit_group),
+            Array(20, self.measurement_group),
+            self.corrections_group,
+            self.onehz_waveform_group,
+            Array(20, self.waveform_group))
+
+        self.mds = Array(self.n_records, self.mds_record)
+        return self.mds
 
 
-#            mds_def(name="beam_sd", fmt="H", size=2),
-#            mds_def(name="beam_centre", fmt="H", size=2),
-#            mds_def(name="beam_amplitude", fmt="H", size=2),
-#            mds_def(name="beam_skew", fmt="h", size=2),
-#            mds_def(name="beam_kurt", fmt="h", size=2),
-#            mds_def(name="beam_spare", fmt="50b", size=50)]
-
-    def get_bytesize(self, msd_group):
-        group = getattr(self, msd_group)
-        return sum([field.size for field in group])
-
-    def get_struct_parser(self, msd_group):
-        group = getattr(self, msd_group)
-        return ">"+"".join([field.fmt for field in group])
-
-    def get_field_names(self, msd_group):
-        group = getattr(self, msd_group)
-        return ([field.name for field in group])
-
-
-def cryosat2_get_msd_def(radar_mode, baseline):
+def cryosat2_get_msd_def(radar_mode, baseline, n_records):
     # XXX: Testing purposes only, needs functionality
-    return Cryosat2L1bMSDDefinitionSarBaselineC()
+
+    definition = Cryosat2L1bMSDDefinition()
+    definition.baseline = baseline
+    definition.radar_mode = radar_mode
+    definition.n_records = n_records
+
+    return definition.get_msd_parser()
