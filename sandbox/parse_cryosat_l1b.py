2dd0d4ca92fc8b5191e0121c6c218d1aeaa9f5d6
Stefan Hendricks <stefan.hendricks@awi.de>
2015-07-14 15:08:33 +0200

    [sandbox] moved functions developed in this script to the cryosat2.function module

--- sandbox/parse_cryosat_l1b.py
+++ sandbox/parse_cryosat_l1b.py
@@ -7,10 +7,12 @@ Created on Tue Jul 07 13:53:17 2015
 
 from pysiral.config import ConfigInfo
 from pysiral.cryosat2.cryosat2_l1b import CryoSatL1B
-
+from pysiral.cryosat2.functions import (
+    get_tai_datetime_from_timestamp, get_cryosat2_wfm_power,
+    get_cryosat2_wfm_range, tai2utc)
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
+
 import glob
 import os
 
@@ -36,57 +38,23 @@ def parse_cryosat_l1b():
 
     # Get timing informatio
     timeorbit = l1b.mds[0].time_orbit[0]
-    time_tai = get_tai_time(timeorbit.day, timeorbit.sec, timeorbit.msec)
+    time_tai = get_tai_datetime_from_timestamp(timeorbit.tai_timestamp)
     time_utc = tai2utc(time_tai)
-    print time_tai
 
     # Get Waveform
     record = l1b.mds[0].waveform[0]
     measurement = l1b.mds[0].measurement[0]
     wfm = np.array(record.wfm).astype(np.float32)
-    wfm_power = get_wfm_power(wfm, record.linear_scale, record.power_scale)
+    wfm_power = get_cryosat2_wfm_power(
+        wfm, record.linear_scale, record.power_scale)
 
     # Calculate range for wavforme
-    wfm_range = get_wfm_range(measurement.window_delay, len(wfm))
+    wfm_range = get_cryosat2_wfm_range(measurement.window_delay, len(wfm))
 
     # Plot the figure
     plt.figure()
     plt.plot(wfm_range, wfm_power)
     plt.show()
-
-
-def get_wfm_power(counts, linear_scale, power_scale):
-    """ Test function to return the waveform power in Watt """
-    return counts*(linear_scale*1e-9)*2.0**(power_scale)
-
-
-def get_wfm_range(window_delay, n_counts):
-    """ Test function to return range for the waveform bins """
-    lightspeed = 299792458.0
-    bandwidth = 320000000.0
-    # The two way delay time give the distance to the central bin
-    central_bin_range = window_delay*lightspeed/2.0
-    # Calculate the offset from the center to the first range bin
-    window_size = (n_counts*lightspeed)/(4.0*bandwidth)
-    first_bin_offset = window_size/2.0
-    # Calculate the range increment for each bin
-    range_increment = np.arange(n_counts)*lightspeed/(4.0*bandwidth)
-    wfm_range = central_bin_range - first_bin_offset + range_increment
-    return wfm_range
-
-
-def get_tai_time(day, sec, msec):
-    return datetime(2000, 1, 1)+timedelta(day, sec, msec)
-
-
-def tai2utc(time_tai):
-    fraction_of_day = float(time_tai.seconds)/86400.
-    offset = pysofa.dat(
-        time_tai.year,
-        time_tai.month,
-        time_tai.day,
-        fraction_of_day)
-    print offset
 
 if __name__ == "__main__":
     parse_cryosat_l1b()
