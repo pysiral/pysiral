3d9945d6da2d83b1e7cf22ccf4498d3c350fa146
Stefan Hendricks <stefan.hendricks@awi.de>
2015-07-23 19:09:36 +0200

    [sandbox] now also plot elevation as range

--- sandbox/cryosat2_to_l1bdata.py
+++ sandbox/cryosat2_to_l1bdata.py
@@ -12,6 +12,7 @@ from pysiral.config import ConfigInfo
 from pysiral.l1bdata import L1bConstructor
 
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from mpl_toolkits.basemap import Basemap
 import numpy as np
 
@@ -38,6 +39,7 @@ def cryosat2_to_l1bdata():
     # Quick plots
     cryosat2_l1b_orbit_plot(l1b)
     cryosat2_l1b_corrections_plot(l1b)
+    cryosat2_l1b_waveform_plot(l1b)
 
 
 def cryosat2_l1b_orbit_plot(l1b):
@@ -91,7 +93,72 @@ def cryosat2_l1b_corrections_plot(l1b):
             ax[i].spines[spine].set_visible(False)
 
     plt.tight_layout()
+    plt.show(block=False)
+
+
+def cryosat2_l1b_waveform_plot(l1b):
+
+    plt.figure(facecolor="white", figsize=(12, 6))
+    ax = plt.gca()
+
+    image, elevation_range = align_echo_power(
+        l1b.waveform.power, l1b.waveform.range, l1b.time_orbit.altitude)
+    image_extent = (0, len(l1b.time_orbit.altitude),
+                    np.amin(elevation_range), np.amax(elevation_range))
+
+    im = ax.imshow(
+        np.log(image).transpose(), cmap=plt.get_cmap("gnuplot2"),
+        interpolation='none', origin='lower', extent=image_extent,
+        aspect=12)
+
+    ax.yaxis.set_tick_params(direction='out')
+    ax.yaxis.set_ticks_position('left')
+    ax.set_ylabel("Elevation (m)")
+    ax.xaxis.set_ticks([])
+    ax.spines["left"].set_position(("data", -20))
+
+    spines_to_remove = ["top", "right", "bottom"]
+    for spine in spines_to_remove:
+        ax.spines[spine].set_visible(False)
+
+    cb = plt.colorbar(im, orientation="vertical", shrink=0.5)
+    cb.set_label("Echo Power log(dB)")
+    plt.tight_layout()
     plt.show()
 
+
+def align_echo_power(power, range, altitude):
+
+    from scipy.interpolate import interp1d
+
+    n_range_bins = len(power[0, :])
+    n_records = len(power[:, 0])
+
+    elevation = np.repeat(altitude, n_range_bins)
+    elevation = elevation.reshape(n_records, n_range_bins)
+    elevation -= range
+
+    range_step = range[0, 1] - range[0, 0]
+
+    elevation_range = np.arange(np.amin(elevation)-range_step*0.5,
+                                np.amax(elevation)+range_step*0.5,
+                                range_step)
+
+    aligned_power = np.ndarray(shape=(n_records, len(elevation_range)))
+    aligned_power *= np.nan
+
+    for i in np.arange(n_records):
+        f = interp1d(elevation[i, :].flatten(), power[i, :].flatten(),
+                     bounds_error=False, fill_value=np.nan)
+        aligned_power[i, :] = f(elevation_range)
+
+    return aligned_power, elevation_range
+
 if __name__ == "__main__":
+    mpl.rcParams['font.sans-serif'] = "arial"
+
+    for target in ["xtick.color", "ytick.color", "axes.edgecolor",
+                   "axes.labelcolor"]:
+        mpl.rcParams[target] = "#4b4b4d"
+
     cryosat2_to_l1bdata()
