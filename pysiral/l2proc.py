ae3c3e7a21806e24b57b4b0884ba9a91280d6736
Stefan Hendricks <stefan.hendricks@awi.de>
2015-07-27 14:23:30 +0200

    [pysiral] Minor edits and added roi subsetting

--- pysiral/l2proc.py
+++ pysiral/l2proc.py
@@ -6,6 +6,7 @@ Created on Fri Jul 24 14:04:27 2015
 """
 from pysiral.l1bdata import L1bConstructor
 from pysiral.l2data import Level2Data
+from pysiral.roi import *
 
 
 class Level2Processor(object):
@@ -14,18 +15,22 @@ class Level2Processor(object):
 
         self._job = job
         self._orbit = []
+        self._l1b_files = []
         self._config = None
         self._error_handler = {"raise_on_error": True}
 
     def error_handling(self, **keyw):
-        self.error_handler.update(keyw)
+        self._error_handler.update(keyw)
 
     def set_config(self, config):
         self._config = config
 
+    def set_l1b_files(self, l1b_files):
+        self._l1b_files = l1b_files
+
     def run(self):
         """ Run the processor """
-        self._initiatize_processor()
+        self._initialize_processor()
         self._run_processor()
         self._clean_up()
 
@@ -44,14 +49,14 @@ class Level2Processor(object):
     def _run_processor(self):
         """ Orbit-wise level2 processing """
         # TODO: Evaluate parallelization
-        for l1b_file in self._job.l1b_files:
+        for l1b_file in self._l1b_files:
             l1b = self._read_l1b_file(l1b_file)
-            in_roi = self._trim_to_roi(l1b)
+            in_roi = self._trim_roi(l1b)
             if not in_roi:
                 continue
             self._range_corrections(l1b)
             l2 = Level2Data()
-            self._classify_surface_type(l1b, l2)
+            self._classify_surface_types(l1b, l2)
             self._retrack_waveforms(l1b, l2)        # -> Elevations
             self._reference_to_ssh(l2)              # -> Radar Freeboard
             self._apply_data_quality_filter(l2)
@@ -67,16 +72,34 @@ class Level2Processor(object):
         """ Read a L1b data file """
         l1b = L1bConstructor(self._config)
         l1b.mission = self._job.mission.id
-        l1b.set_mission_options(self._job.mission.options)
+        l1b.set_mission_options(**self._job.mission.options)
         l1b.filename = l1b_file
         l1b.construct()
         return l1b
 
     def _trim_roi(self, l1b):
-        """ Trims orbit for ice/ocean areas """
+        """
+        Trims orbit for ice/ocean areas and returns true if data in ROI and
+        false otherwise
+        """
+        classifier = globals()[self._job.roi.pyclass]()
+        classifier.set_options(**self._job.roi.options)
+        roi_list = classifier.get_roi_list(
+            l1b.time_orbit.longitude, l1b.time_orbit.latitude)
+        if len(roi_list) == 0:  # No match
+            return False
+        if len(roi_list) == l1b.n_records:  # Full match (no trimming)
+            return True
+        l1b.trim_to_subset(roi_list)    # Partial match (trimming)
         return True
 
-    def _retrack_waveforms(self, l1b):
+    def _range_corrections(self, l1b):
+        pass
+
+    def _classify_surface_types(self, l1b, l2):
+        pass
+
+    def _retrack_waveforms(self, l1b, l2):
         pass
 
     def _reference_to_ssh(self, l2):
