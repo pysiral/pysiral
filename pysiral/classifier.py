534c02ac04ebe846b89c94b42823603ca0a26f50
Stefan Hendricks <stefan.hendricks@awi.de>
2015-07-28 11:26:10 +0200

    [pysiral] (classifier) Added CryoSat-2 pulse peakiness legacy classifier

--- pysiral/classifier.py
+++ pysiral/classifier.py
@@ -44,3 +44,48 @@ class OCOGParameter(BaseClassifier):
     @property
     def width(self):
         return self._width
+
+
+class CS2PulsePeakiness(BaseClassifier):
+    """
+    Calculates Pulse Peakiness (full, left & right) for CryoSat-2 waveform
+    counts
+    XXX: This is a 1 to 1 legacy implementation of the IDL CS2AWI method,
+         consistent method of L1bData or L2Data is required
+    """
+    def __init__(self, wfm_counts, pad=2):
+        super(CS2PulsePeakiness, self).__init__()
+        shape = np.shape(wfm_counts)
+        self._n = shape[0]
+        self._n_range_bins = shape[1]
+        self._pad = pad
+        self._peakiness = np.ndarray(shape=(self._n))*np.nan
+        self._peakiness_r = np.ndarray(shape=(self._n))*np.nan
+        self._peakiness_l = np.ndarray(shape=(self._n))*np.nan
+        self._calc_parameters(wfm_counts)
+
+    def _calc_parameters(self, wfm_counts):
+        for i in np.arange(self._n):
+            y = wfm_counts[i, :].flatten().astype(np.float32)
+            y -= np.nanmean(y[0:11])  # Remove Noise
+            y[np.where(y < 0.0)[0]] = 0.0  # Set negative counts to zero
+            yp = np.nanmax(y)  # Waveform peak value
+            ypi = np.nanargmax(y)  # Waveform peak index
+            if ypi > 3*self._pad and ypi < self._n_range_bins-4*self._pad:
+                self._peakiness_l[i] = yp/np.nanmean(
+                    y[ypi-3*self._pad:ypi-1*self._pad+1])*3.0
+                self._peakiness_r[i] = yp/np.nanmean(y[
+                    ypi+1*self._pad:ypi+3*self._pad+1])*3.0
+                self._peakiness[i] = yp/y.sum()*self._n_range_bins
+
+    @property
+    def peakiness(self):
+        return self._peakiness
+
+    @property
+    def peakiness_r(self):
+        return self._peakiness_r
+
+    @property
+    def peakiness_l(self):
+        return self._peakiness_l
