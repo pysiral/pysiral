16fb0c79dd95ff2fcf09b7c770517934f58902f3
Stefan Hendricks <stefan.hendricks@awi.de>
2015-07-28 17:39:39 +0200

    [pysiral] (l2data) Added handling of surface_type information

--- pysiral/l2data.py
+++ pysiral/l2data.py
@@ -4,10 +4,13 @@ Created on Fri Jul 24 16:30:24 2015
 
 @author: Stefan
 """
+import numpy as np
 
 
 class Level2Data(object):
 
     def __init__(self, l1b):
+        self.info = np.copy(l1b.info)
 
-        self._info = l1b.info
+    def set_surface_type(self, surface_type):
+        self.surface_type = surface_type
