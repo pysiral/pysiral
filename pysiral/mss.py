2ad0df21e96b4182a66297c154d7656f289cc1de
Stefan Hendricks <stefan.hendricks@awi.de>
2015-08-02 15:36:18 +0200

    [pysiral] (mss) Finished ssa computation of SSASmoothedLinear, uncertainty computation still misssing

--- pysiral/mss.py
+++ pysiral/mss.py
@@ -105,44 +105,81 @@ class SSASmoothedLinear(SSAInterpolator):
         super(SSASmoothedLinear, self).__init__()
 
     def _interpolate(self, l2):
-        self._linear_interpolation_between_leads(l2)
+        self._linear_smoothed_interpolation_between_tiepoints(l2)
         self._calculate_uncertainty()
-        self._smooth_interpolated_ssa()
 
-    def _linear_interpolation_between_leads(self, l2):
-        import matplotlib.pyplot as plt
+    def _linear_smoothed_interpolation_between_tiepoints(self, l2):
         # Use ocean and lead elevations
-        ssh_tiepoints = l2.surface_type.lead.indices
+        self._ssh_tiepoints = l2.surface_type.lead.indices
         if self._options.use_ocean_wfm:
-            ssh_tiepoints.append(l2.surface_type.ocean.indices)
-            ssh_tiepoints = np.sort(ssh_tiepoints)
-        # Interpolate
+            self._ssh_tiepoints.append(l2.surface_type.ocean.indices)
+            self._ssh_tiepoints = np.sort(self._ssh_tiepoints)
+        # Get initial elevation at tiepoint locations
+        mss_frb = l2.elev - l2.mss
         ssa_raw = np.ndarray(shape=(l2.n_records))*np.nan
-        ssa_raw[ssh_tiepoints] = l2.elev[ssh_tiepoints]
-        # Calculate filter width
-        filter_width = self._options.smooth_filter_width_m/300.0
+        ssa_raw[self._ssh_tiepoints] = mss_frb[self._ssh_tiepoints]
+        non_tiepoints = np.where(np.isnan(ssa_raw))
+        # Prepare filtering, get filter width
+        #   filter width need to in points
+        #   -> need typical footprint size to be defined in filter options
+        filter_width = self._options.smooth_filter_width_m / \
+            self._options.smooth_filter_width_footprint_size
+        # Make sure filter width is odd number
         filter_width = np.floor(filter_width) // 2 * 2 + 1
-        print filter_width
+        filter_width = filter_width.astype(int)
+        # Filtered raw values
+        # Use custom implementation of IDL SMOOTH:
+        # idl_smooth(x, w) equivalent to SMOOTH(x, w, /edge_truncate, /nan)
+        ssa_filter1 = idl_smooth(ssa_raw, filter_width)
+        # Leave only the original ssh tie points
+        ssa_filter1[non_tiepoints] = np.nan
+        # Fill nans with linear interpolation and contant values at borders
+        # python: fill_nan(x) = IDL: FILL_NAN(x, /NEIGHBOUR)
+        ssa_filter2 = fill_nan(ssa_filter1)
+        # Final smoothing
+        ssa = idl_smooth(ssa_filter2, filter_width)
+        self._value = ssa
 
-        x = np.arange(l2.n_records)
-        plt.figure(facecolor="white")
-        plt.scatter(x, ssa_raw)
-        plt.scatter(x, idl_smooth(ssa_raw, filter_width), color="red")
-        plt.show()
-
-
-        raise Exception()
-
+        # TODO: Make example plot of individual filter steps
+#        import matplotlib.pyplot as plt
 #        x = np.arange(l2.n_records)
-#        interpolator = interp1d(x[ssh], l2.elev[ssh],
-#                                kind='linear', bounds_error=False)
-#        self._value = interpolator(x)
+#        plt.figure(facecolor="white")
+#        plt.scatter(x, ssa_raw, color="black")
+#        plt.scatter(x, ssa_filter1, color="red", alpha=0.5)
+#        plt.plot(x, ssa_filter2, color="blue", lw=2, alpha=0.5)
+#        plt.plot(x, ssa, color="orange", lw=3)
+#        plt.show()
 
     def _calculate_uncertainty(self):
         pass
 
-    def _smooth_interpolated_ssa(self):
-        pass
+#      IDL Code for getting ssh uncertainty
+#      STD = REPLICATE(!VALUES.F_NAN, CS_L1B.N_RECORDS)
+#      FOR I=0,CS_L1B.N_RECORDS-1 DO BEGIN
+#        IF I LT SM/2 THEN BEGIN
+#          TMP = WHERE(FINITE(SSHA_RAW[0:I+SM/2]) EQ 1, NN)
+#          IF NN GT 0 THEN STD[I] = 1/SQRT(NN) * STDDEV([REPLICATE(!VALUES.D_NAN,ABS(I-SM/2)),SSHA_RAW[0:I+SM/2]],/NAN)
+#          NN = 0
+#        ENDIF
+#        IF I GE SM/2 AND I LE CS_L1B.N_RECORDS-1-SM/2 THEN BEGIN
+#          TMP = WHERE(FINITE(SSHA_RAW[I-SM/2:I+SM/2]) EQ 1, NN)
+#          IF NN GT 0 THEN STD[I] = 1/SQRT(NN) * STDDEV(SSHA_RAW[I-SM/2:I+SM/2],/NAN)
+#          NN = 0
+#        ENDIF
+#        IF I GT CS_L1B.N_RECORDS-1-SM/2 THEN BEGIN
+#          TMP = WHERE(FINITE(SSHA_RAW[I-SM/2:CS_L1B.N_RECORDS-1]) EQ 1, NN)
+#          IF NN GT 0 THEN STD[I] = 1/SQRT(NN) * STDDEV([SSHA_RAW[I-SM/2:CS_L1B.N_RECORDS-1],REPLICATE(!VALUES.D_NAN,ABS(CS_L1B.N_RECORDS-1+SM/2))],/NAN)
+#          NN = 0
+#        ENDIF
+#      ENDFOR
+#
+#      TMP = WHERE(FINITE(STD,/NAN),NN)
+#      IF NN GT 0 THEN BEGIN
+#        STD_MAX = ABS(SSHA_VALID - SMOOTH(ELEVATION,SM,/NAN,/EDGE_TRUNCATE))
+#        STD[TMP] = STD_MAX[TMP]
+#      ENDIF
+#
+#      SSH_UNCERTAINTY = SMOOTH(STD,SM,/NAN,/EDGE_TRUNCATE)
 
 
 def egm2wgs_delta_h(phi):
@@ -199,6 +236,54 @@ def idl_smooth(x, window):
             kernel_halfsize = i
         if (n-1-i < kernel_halfsize):
             kernel_halfsize = n-1-i
-        print kernel_halfsize
         smoothed[i] = np.nanmean(x[i-kernel_halfsize:i+kernel_halfsize+1])
     return smoothed
+
+
+# TODO: Custom implementation of a gaussian weighted smoother?
+def gauss_smooth(x, window):
+    pass
+#    from astropy.convolution import Gaussian1DKernel
+#    smoothed = np.copy(x)*np.nan
+#    # get a gaussi
+#    kernel = Gaussian1DKernel(10)
+#    n = len(x)
+#    for i in np.arange(n):
+#        kernel_halfsize = np.floor((window-1)/2).astype(int)
+#        if (i < kernel_halfsize):
+#            kernel_halfsize = i
+#        if (n-1-i < kernel_halfsize):
+#            kernel_halfsize = n-1-i
+#        smoothed[i] = np.nanmean(x[i-kernel_halfsize:i+kernel_halfsize+1])
+#    return smoothed
+
+
+def spline_smooth(y, window):
+    from scipy.interpolate import UnivariateSpline
+    x = np.arange(len(y))
+    no_nan = np.where(np.isfinite(y))
+    spl = UnivariateSpline(x[no_nan], y[no_nan], s=len(y)/window, k=4)
+    spl.set_smoothing_factor(0.25)
+    return spl(x)
+
+
+def fill_nan(y):
+    result = np.copy(y)
+    # Get first and last valid index
+    no_nan = np.where(np.isfinite(y))[0]
+    valid0, valid1 = np.amin(no_nan), np.amax(no_nan)
+    # Cut the "inside" section that is bounded by valid measurements
+    y_inside = y[valid0:valid1+1]
+    x = np.arange(len(y_inside))
+    valid_inside = np.where(np.isfinite(y_inside))[0]
+    # Interpolate inside range of valid entries
+    func = interp1d(x[valid_inside], y_inside[valid_inside],
+                    bounds_error=False)
+    interpolated_inside = func(x)
+    result[valid0:valid1+1] = interpolated_inside
+    # fill nan-borders with first/last valid value
+    if valid0 != 0:
+        result[0:valid0] = y[valid0]
+    if valid1 != len(y)-1:
+        result[valid1+1:len(y)+1] = y[valid1]
+    return result
