af2d2643dc0d0667d0e624ccc792f3782fd0f63b
Stefan Hendricks <stefan.hendricks@awi.de>
2016-01-18 15:19:56 +0100

    [maptools] Adding maptools module. Initial commit with constructor class for grid information (corner coordinates from center coordinates) for pcolor plots

---
+++ pysiral/maptools.py
@@ -0,0 +1,42 @@
+import numpy as np
+from pyproj import Proj
+
+
+class GeoPcolorGrid():
+
+    def __init__(self, lon, lat):
+
+        self.n, self.m = np.shape(lon)
+        self.glon = lon
+        self.glat = lat
+        self.longitude = np.ndarray((self.n+1, self.m+1), dtype=np.float32)
+        self.latitude = np.ndarray((self.n+1, self.m+1), dtype=np.float32)
+
+    def calc_from_proj(self, **kwargs):
+
+        p = Proj(**kwargs)
+        x, y = p(self.glon, self.glat)
+
+        xp = np.ndarray((self.n+1, self.m+1), dtype=np.float32)
+        yp = np.ndarray((self.n+1, self.m+1), dtype=np.float32)
+
+        dx = -0.5 * (x[0, 1] - x[0, 0])
+        dy = -0.5 * (y[1, 0] - y[0, 0])
+
+        # Main part
+        xp[0:self.n, 0:self.m] = x[0:self.n, 0:self.m] + dx
+        yp[0:self.n, 0:self.m] = y[0:self.n, 0:self.m] + dy
+
+        # lower boundary
+        xp[self.n, 0:self.m] = x[self.n-1, 0:self.m] + dx
+        yp[self.n, 0:self.m] = y[self.n-1, 0:self.m] - dy
+
+        # Right boundary
+        xp[0:self.n, self.m] = x[0:self.n, self.m-1] - dx
+        yp[0:self.n, self.m] = y[0:self.n, self.m-1] + dy
+
+        # Last tiny piece
+        xp[self.n, self.m] = x[self.n-1, self.m-1] - dx
+        yp[self.n, self.m] = y[self.n-1, self.m-1] - dy
+
+        self.longitude, self.latitude = p(xp, yp, inverse=True)
