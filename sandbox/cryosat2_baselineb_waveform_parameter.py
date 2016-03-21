09b4eaa5e4539aae407cd02836c352bca15ca0d1
Stefan Hendricks <stefan.hendricks@awi.de>
2016-03-21 20:16:23 +0100

    [sandbox] rewritten to parse several files for a given region of interest

--- sandbox/cryosat2_baselineb_waveform_parameter.py
+++ sandbox/cryosat2_baselineb_waveform_parameter.py
@@ -33,45 +33,53 @@ def cryosat2_baselineb_waveform_parameter():
     l1b_directory = os.path.join(l1b_directory, "2013", "12")
     l1b_files = glob.glob(os.path.join(l1b_directory, "*.DBL"))
 
-    # Read the file
-    l1b = L1bConstructor(config)
-    l1b.mission = "cryosat2"
-    l1b.filename = l1b_files[0]
-    l1b.construct()
-
-    # Write to netcdf group
-    output_folder = r"D:\awi\product\altimetry\pysiral\tests\l1b_ncfiles"
+    # netcdf export
     export_datagroups = ["time_orbit", "waveform", "classifier"]
-    ncfile = L1bNCfile()
-    ncfile.zlib = True
-    ncfile.l1b = l1b
-    ncfile.datagroups = export_datagroups
-    ncfile.output_folder = output_folder
-    ncfile.export()
+    output_folder = r"D:\awi\product\altimetry\pysiral\tests\l1b_ncfiles"
+
+    # Region of interest settings
+    roi_definition = LatitudeLongitudeBox()
+    roi_definition.set_options(lat_range=[-88., -50.], lon_range=[-30., 40.])
+
+    # loop over iput files
+    for l1b_filename in l1b_files:
+        # Parse L1b files
+        l1b = L1bConstructor(config)
+        l1b.mission = "cryosat2"
+        l1b.filename = l1b_filename
+        l1b.construct()
+        # Check region of interest
+        l1b_antarctic_subset = l1b.extract_region_of_interest(roi_definition)
+        # Validate overlap
+        if l1b_antarctic_subset.info.n_records == -1:
+            continue
+        # Write to l1b netcdf container file
+        ncfile = L1bNCfile()
+        ncfile.l1b = l1b_antarctic_subset
+        ncfile.datagroups = export_datagroups
+        ncfile.output_folder = output_folder
+        ncfile.export()
 
     # Test plot of netcdf content
     filename = os.path.join(
         r"D:\awi\product\altimetry\pysiral\tests\l1b_ncfiles",
-        r"CS_OFFL_SIR_SAR_1B_20131201T003739_20131201T004843_B001.nc")
+        r"CS_OFFL_SIR_SAR_1B_20131201T012220_20131201T012916_B001.nc")
     nc = Dataset(filename, "r")
     wfm_power = nc.groups["waveform"].variables["power"][:]
     wfm_range = nc.groups["waveform"].variables["range"][:]
     altitude = nc.groups["time_orbit"].variables["altitude"][:]
-    ssd = nc.groups["classifier"].variables["stack_standard_deviation"][:]
+    longitude = nc.groups["time_orbit"].variables["longitude"][:]
+    latitude = nc.groups["time_orbit"].variables["latitude"][:]
     nc.close()
-
-    plt.figure()
-    plt.plot(ssd)
-    plt.show()
 
     # Test plot
     cryosat2_l1b_waveform_plot(wfm_power, wfm_range, altitude)
-
+    l1b_orbit_plot(longitude, latitude)
 
 
 def cryosat2_l1b_waveform_plot(wfm_power, wfm_range, altitude):
 
-    plt.figure(facecolor="white", figsize=(12, 6))
+    plt.figure("Waveform Quickview", facecolor="white", figsize=(12, 6))
     ax = plt.gca()
 
     image, elevation_range = align_echo_power(
@@ -82,7 +90,7 @@ def cryosat2_l1b_waveform_plot(wfm_power, wfm_range, altitude):
     im = ax.imshow(
         np.log(image).transpose(), cmap=plt.get_cmap("magma"),
         interpolation='none', origin='lower', extent=image_extent,
-        aspect=12)
+        aspect=35)
 
     ax.yaxis.set_tick_params(direction='out')
     ax.yaxis.set_ticks_position('left')
@@ -98,6 +106,38 @@ def cryosat2_l1b_waveform_plot(wfm_power, wfm_range, altitude):
     cb.set_label("Echo Power log(dB)")
     plt.tight_layout()
     plt.show()
+
+
+def l1b_orbit_plot(longitude, latitude):
+
+    # AWI eisblau #00ace5
+    # AWI tiefblau #003e6e
+    # AWI grau 1 #4b4b4d
+    # AWI grau 2 #bcbdbf
+
+    grid_keyw = {"dashes": (None, None),
+                 "color": "#bcbdbf",
+                 "linewidth": 0.5,
+                 "latmax": 88}
+
+    lat_0 = np.median(latitude)
+    lon_0 = np.median(longitude)
+
+#    if np.nanmean(l1b.time_orbit.latitude) < 0:
+#        lat_0 *= -1.0
+
+    plt.figure("Orbit Quickview", facecolor="white")
+    m = Basemap(projection='ortho', lon_0=lon_0, lat_0=lat_0, resolution='i')
+    m.fillcontinents(color='#00ace5', lake_color='#00ace5')
+    # draw parallels and meridians.
+    m.drawparallels(np.arange(-90., 120., 10.), **grid_keyw)
+    m.drawmeridians(np.arange(0., 420., 15.), **grid_keyw)
+
+    x, y = m(longitude, latitude)
+    m.plot(x, y, color="#003e6e", linewidth=2.0)
+
+    plt.title("")
+    plt.show(block=False)
 
 
 def align_echo_power(power, range, altitude):
