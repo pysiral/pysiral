0c62eb6f7f43a28fe43d3b0487990ea5ed0350fb
Stefan Hendricks <stefan.hendricks@awi.de>
2016-03-21 23:28:37 +0100

    [sandbox] Added transparency to the full warpimage

--- sandbox/grid_visualization_presentation.py
+++ sandbox/grid_visualization_presentation.py
@@ -39,14 +39,15 @@ def grid_visualization_presentation():
 #    print imarr.dtype
 #    stop
 
+    # template_image = Image.new("RGBA", (1535, 768), (0, 0, 0, 255))
     template_image = Image.new("RGBA", (1024, 512), (0, 0, 0, 255))
     shading_image_array = np.array(template_image)
     for i in range(256):
-        shading_image_array[i, :, :] = [i, 0, 0, i]
+        shading_image_array[i, :, :] = [255-i, 255-i, 255-i, 255]
     shading_image = Image.fromarray(shading_image_array)
-    plt.figure()
-    plt.imshow(shading_image)
-    plt.show(block=True)
+#    plt.figure()
+#    plt.imshow(shading_image)
+#    plt.show(block=True)
     png_info = shading_image.info
     shading_image.save('temp.png', 'PNG', **png_info)
 
@@ -54,21 +55,24 @@ def grid_visualization_presentation():
     grid_keyw = {"dashes": (None, None),
                  "color": "#bcbdbf",
                  "linewidth": 0.5,
-                 "latmax": 85}
+                 "latmax": 85,
+                 "zorder": 120}
 
-    lat_0 = 85
-    lon_0 = -45
+    lat_0 = 80
+    lon_0 = 0
 
     plt.figure("Grid Data Visualization", facecolor="white")
-    h = 6000.
-    m = Basemap(projection='nsper', lon_0=lon_0, lat_0=lat_0,
-                satellite_height=h*1000., resolution='l')
+    h = 6500.
+#    m = Basemap(projection='nsper', lon_0=lon_0, lat_0=lat_0,
+#                satellite_height=h*1000., resolution='l')
+    m = Basemap(projection='ortho', lon_0=lon_0, lat_0=lat_0,
+                resolution='l')
     m.drawmapboundary(fill_color='#003e6e')
     m.fillcontinents(color='#4b4b4d', lake_color='#4b4b4d')
-    coastlines = get_landcoastlines(m, color="#bcbdbf", linewidth=0.5)
+    coastlines = get_landcoastlines(m, color="#bcbdbf", linewidth=1.0)
     plt.gca().add_collection(coastlines)
     # draw parallels and meridians.
-    im = m.warpimage("temp.png", scale=1.0, zorder=100)
+    im = m.warpimage("temp.png", scale=1.0, zorder=100, alpha=0.55)
     # cmBlueMarble(m, basesize="10800x5400", scale=1.0)
     m.drawparallels(np.arange(-90., 120., 15.), **grid_keyw)
     m.drawmeridians(np.arange(0., 420., 30.), **grid_keyw)
