7eb39cc9729418d0710cef8ea4e84597fe56acad
Stefan Hendricks <stefan.hendricks@awi.de>
2016-03-22 18:13:12 +0100

    [sandbox] Rewrote the generation of the shaded background image (equal cylindrical projection with land/sea colors, shading of this "image" by merging with alpha layer, painting on map using warpimage)

--- sandbox/grid_visualization_presentation.py
+++ sandbox/grid_visualization_presentation.py
@@ -14,6 +14,8 @@ from matplotlib.collections import LineCollection
 from mpl_toolkits.basemap import Basemap
 
 import os
+from PIL import Image
+
 
 from plib.map.basemap import cmBlueMarble
 from matplotlib.image import pil_to_array
@@ -27,56 +29,95 @@ from matplotlib.image import pil_to_array
 
 def grid_visualization_presentation():
 
-    from PIL import Image
-
-#    folder = r"D:\awi\workspace\python-tools\plib\map\bluemarble\8100x4050"
-#    filename = r"world.topo.200401.3x8100x4050.jpg"
-#    image_test_file = os.path.join(folder, filename)
-#    im = Image.open(image_test_file)
-#    im = im.convert('RGBA')
-#    imarr = np.array(im)
-#    print imarr.shape
-#    print imarr.dtype
-#    stop
-
-    template_image = Image.new("RGBA", (1535, 768), (0, 0, 0, 255))
-    # template_image = Image.new("RGBA", (1024, 512), (0, 0, 0, 255))
-    shading_image_array = np.array(template_image)
-    for i in range(256):
-        shading_image_array[i, :, :] = [255-i, 255-i, 255-i, 255]
-    shading_image = Image.fromarray(shading_image_array)
-#    plt.figure()
-#    plt.imshow(shading_image)
-#    plt.show(block=True)
-    png_info = shading_image.info
-    shading_image.save('temp.png', 'PNG', **png_info)
+    create_background_image()
+    create_map()
 
 
-    grid_keyw = {"dashes": (None, None),
-                 "color": "#bcbdbf",
-                 "linewidth": 0.5,
-                 "latmax": 85,
-                 "zorder": 120}
+def create_background_image():
 
+    plt.figure("Background Image", figsize=(12, 6))
+    plt.gca().set_position([0, 0, 1, 1])
+    m = Basemap(projection='cyl', llcrnrlat=-90, urcrnrlat=90,
+                llcrnrlon=-180, urcrnrlon=180, resolution='i')
+    m.drawmapboundary(color="none", fill_color='#003e6e')
+    m.fillcontinents(color='#4b4b4d', lake_color='#4b4b4d')
+    coastlines = get_landcoastlines(m, color="#bcbdbf", linewidth=0.05)
+    plt.gca().add_collection(coastlines)
+    plt.savefig("temp.png", dpi=1200)
+    plt.close()
+
+    background = Image.open("temp.png")
+    foreground_array = get_foreground_array(background.size)
+    foreground_array_wet = get_foreground_array(background.size, cutoff=0.8)
+#    mask = np.array(background)
+#    wet_pixel_list = np.where(mask == [0, 62, 110, 255])
+#    foreground_array[wet_pixel_list] = foreground_array_wet[wet_pixel_list]
+#    foreground_array[wet_pixel_list] = [100, 62, 110, 255]
+    foreground = Image.fromarray(foreground_array)
+    background.paste(foreground, (0, 0), foreground)
+    background.save('temp2.png', 'PNG')
+
+
+def get_foreground_array(size, cutoff=0.5, color=[0, 0, 0], alpha_ref=200):
+    foreground_template = Image.new("RGBA", size, (0, 0, 0, 200))
+    foreground_array = np.array(foreground_template)
+    latimin, latimax = 0, int(cutoff*size[1])
+    scale_factor = 256./np.float(latimax)
+    for i in np.arange(latimin, latimax).astype(int):
+        alpha = int(i*scale_factor)
+        if alpha > 200:
+            alpha = 200
+        if alpha < 0:
+            alpha = 0
+        foreground_array[i, :, :] = [0, 0, 0, alpha]
+    return foreground_array
+
+
+def create_map():
     lat_0 = 80
     lon_0 = 0
-
-    plt.figure("Grid Data Visualization", facecolor="#4b4b4d")
     h = 5000.
+    grid_keyw = {"dashes": (None, None), "color": "#ffffff",
+                 "linewidth": 0.05, "latmax": 85, "zorder": 120}
+    plt.figure("Grid Data Visualization",  dpi=300, facecolor="#4b4b4d")
     m = Basemap(projection='nsper', lon_0=lon_0, lat_0=lat_0,
                 satellite_height=h*1000., resolution='l')
-#    m = Basemap(projection='ortho', lon_0=lon_0, lat_0=lat_0,
-#                resolution='l')
-    m.drawmapboundary(fill_color='#003e6e')
-    m.fillcontinents(color='#4b4b4d', lake_color='#4b4b4d')
-    coastlines = get_landcoastlines(m, color="#bcbdbf", linewidth=1.0)
-    plt.gca().add_collection(coastlines)
-    # draw parallels and meridians.
-    im = m.warpimage("temp.png", scale=1.0, zorder=100, alpha=0.4)
-    # cmBlueMarble(m, basesize="10800x5400", scale=1.0)
+    m.warpimage("temp2.png", scale=1.0, zorder=100)
     m.drawparallels(np.arange(-90., 120., 15.), **grid_keyw)
     m.drawmeridians(np.arange(0., 420., 30.), **grid_keyw)
     plt.show()
+
+
+def fig2data(fig):
+    """
+    @brief Convert a Matplotlib figure to a 4D numpy array with RGBA channels
+           and return it
+    @param fig a matplotlib figure
+    @return a numpy 3D array of RGBA values
+    """
+    # draw the renderer
+    fig.canvas.draw()
+    # Get the RGBA buffer from the figure
+    w, h = fig.canvas.get_width_height()
+    buf = np.fromstring(fig.canvas.tostring_argb(), dtype=np.uint8)
+    buf.shape = (w, h, 4)
+    # canvas.tostring_argb give pixmap in ARGB mode.
+    # Roll the ALPHA channel to have it in RGBA mode
+    buf = np.roll(buf, 3, axis=2)
+    return buf
+
+
+def fig2img(fig):
+    """
+    @brief Convert a Matplotlib figure to a PIL Image in RGBA format
+           and return it
+    @param fig a matplotlib figure
+    @return a Python Imaging Library ( PIL ) image
+    """
+    # put the figure pixmap into a numpy array
+    buf = fig2data(fig)
+    w, h, d = buf.shape
+    return Image.fromstring("RGBA", (w, h), buf.tostring())
 
 
 def get_landcoastlines(basemap, color="0.0", linewidth=1):
