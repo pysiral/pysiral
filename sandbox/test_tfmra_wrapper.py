a9a2ce0d6974abbe49042d6e7b9fc345641260ae
Stefan Hendricks <stefan.hendricks@awi.de>
2016-03-29 14:31:47 +0200

    [sandbox] Changed the output of test_tfmra_wrapper

--- sandbox/test_tfmra_wrapper.py
+++ sandbox/test_tfmra_wrapper.py
@@ -7,6 +7,7 @@ Created on Tue Oct 27 11:32:49 2015
 
 import os
 import glob
+import cPickle as pickle
 
 import numpy as np
 
@@ -19,10 +20,10 @@ import matplotlib.pyplot as plt
 
 
 def test_tfmra_wrapper():
-    # Get Configuration Information
+    # # Get Configuration Information
     config = ConfigInfo()
 
-    # Get an L1B SAR file
+    # # Get an L1B SAR file
     l1b_directory = config.local_machine.l1b_repository.cryosat2.sar
     l1b_directory = os.path.join(l1b_directory, "2015", "04")
     l1b_files = glob.glob(os.path.join(l1b_directory, "*.DBL"))
@@ -33,13 +34,22 @@ def test_tfmra_wrapper():
     l1b.filename = l1b_files[0]
     l1b.construct()
 
-    retracker_range = np.ndarray(shape=(l1b.n_records), dtype=np.float32)
-    # This is the relevant call for the retracker wrapper funtion
-    for index in np.arange(l1b.n_records):
-        retracker_range[index] = pytfmra(l1b.waveform.range[index, :],
-                                         l1b.waveform.power[index, :])
+    pickle_file = r"test_tfmra_l1b_object.pickle"
+    with open(pickle_file, "wb") as fh:
+        pickle.dump(
+            [l1b.n_records, l1b.waveform.range, l1b.waveform.power], fh)
 
-    elevation = l1b.time_orbit.altitude - retracker_range
+    with open(pickle_file, "r") as fh:
+        l1b_n_records, l1b_waveform_range, l1b_waveform_power = \
+            pickle.load(fh)
+
+    retracker_range = np.ndarray(shape=(l1b_n_records), dtype=np.float32)
+    # This is the relevant call for the retracker wrapper funtion
+    for index in np.arange(l1b_n_records):
+        retracker_range[index] = pytfmra(l1b_waveform_range[index, :],
+                                         l1b_waveform_power[index, :])
+
+    elevation = retracker_range
     plt.figure()
     plt.plot(elevation)
     plt.show()
