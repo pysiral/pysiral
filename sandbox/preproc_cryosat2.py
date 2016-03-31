db1a41c8fc9a1c7d77016409f948067f450d033a
Stefan Hendricks <stefan.hendricks@awi.de>
2016-03-31 18:37:09 +0200

    [sandbox] minor style edits and comments

--- sandbox/preproc_cryosat2.py
+++ sandbox/preproc_cryosat2.py
@@ -41,10 +41,12 @@ Concatinating orbit files (CryoSat-2 specific):
 file where strange things are happening:
 
 over southern ocean but no has_ocean => false
-D:\awi\altim\data\altimetry\cryosat2\baseline-c\SIR_SAR_L1\2015\03\CS_LTA__SIR_SAR_1B_20150301T025222_20150301T025526_C001.DBL
+D:\awi\altim\data\altimetry\cryosat2\baseline-c\SIR_SAR_L1\2015\03\...
+ ...CS_LTA__SIR_SAR_1B_20150301T025222_20150301T025526_C001.DBL
 
 potential wrong lat/lon:
-D:\awi\altim\data\altimetry\cryosat2\baseline-c\SIR_SAR_L1\2015\03\CS_LTA__SIR_SAR_1B_20150301T032811_20150301T033251_C001.DBL
+D:\awi\altim\data\altimetry\cryosat2\baseline-c\SIR_SAR_L1\2015\03\...
+ ...CS_LTA__SIR_SAR_1B_20150301T032811_20150301T033251_C001.DBL
 
 """
 
@@ -256,12 +258,13 @@ def l1b_is_connected_to_stack(l1b, l1b_stack):
 #    not_ocean = np.logical_not(ocean.flag)
 #
 #
-##    first_land_occurance = list(is_land).index(True)
-##    last_land_
-##    stop
+#    first_land_occurance = list(is_land).index(True)
+#    last_land_
+#    stop
 
 
 def get_first_array_index(array, value):
+    """ Get the index in array of the first occurance of ``value`` """
     try:
         index = list(array).index(value)
     except:
@@ -270,6 +273,7 @@ def get_first_array_index(array, value):
 
 
 def get_last_array_index(array, value):
+    """ Get the index in array of the last occurance of ``value`` """
     listarray = list(array)
     try:
         index = (len(listarray) - 1) - listarray[::-1].index(value)
