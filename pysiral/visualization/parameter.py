b4dd397c591180a48d62e79e5ef8e97005914d2c
Stefan Hendricks <stefan.hendricks@awi.de>
2016-04-07 23:08:47 +0200

    [visualization] Added data container for difference plots that mimics a default parameter container

--- pysiral/visualization/parameter.py
+++ pysiral/visualization/parameter.py
@@ -6,6 +6,7 @@ Created on Thu Apr 07 16:19:41 2016
 """
 
 
+class GridMapParameterBase(object):
     """
     Contains data, pcolor grid calculation capability, colormap definition
     and standardized parameter naming
@@ -21,18 +22,47 @@ Created on Thu Apr 07 16:19:41 2016
         self.pgrid = None
         self.pardef = None
 
-    def get_label(self):
-        return self.pardef.label+" ("+self.pardef.unit+")"
-
     def set_grid(self, longitude, latitude):
         self.longitude = longitude
         self.latitude = latitude
-
-    def set_parameter(self, grid, parameter_name):
-        self.grid = grid
-        self.pardef = self._parameter_definitions[parameter_name]
 
     def set_projection(self, **projection):
         from pysiral.maptools import GeoPcolorGrid
         self.pgrid = GeoPcolorGrid(self.longitude, self.latitude)
         self.pgrid.calc_from_proj(**projection)
+
+
+class GridMapParameter(GridMapParameterBase):
+
+    def __init__(self):
+        super(GridMapParameter, self).__init__()
+
+    def get_cmap(self):
+        return self.pardef.cmap
+
+    def get_label(self):
+        return self.pardef.label+" ("+self.pardef.unit+")"
+
+    def set_parameter(self, grid, parameter_name):
+        self.grid = grid
+        self.pardef = self._parameter_definitions[parameter_name]
+
+
+class GridMapDiffParameter(GridMapParameterBase):
+    """
+    Contains data, pcolor grid calculation capability, colormap definition
+    and standardized parameter naming
+    """
+
+    def __init__(self):
+        super(GridMapDiffParameter, self).__init__()
+
+    def get_cmap(self):
+        return self.pardef.cmap_diff
+
+    def get_label(self):
+        return "$\Delta$ "+self.pardef.label+" ("+self.pardef.unit+")"
+
+    def set_parameter(self, grida, gridb, parameter_name):
+        self.grid = gridb-grida
+        self.pardef = self._parameter_definitions[parameter_name]
