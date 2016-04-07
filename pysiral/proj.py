d9245c95ed6f6de3dbf15f77598f67e90086fce6
Stefan Hendricks <stefan.hendricks@awi.de>
2016-04-07 23:02:55 +0200

    [proj] Added module for projections

---
+++ pysiral/proj.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 07 20:02:58 2016
+
+@author: Stefan
+"""
+
+
+class BaseProjection(object):
+
+    def __init__(self):
+        self.projection = {}
+
+    @property
+    def projection_keyw(self):
+        return self.projection
+
+
+class EASE2North(BaseProjection):
+
+    def __init__(self):
+        super(EASE2North, self).__init__()
+        self.test = "a"
+        self.projection = {
+            "proj": "laea",
+            "lat_0": -90.0,
+            "lon_0": 0.0,
+            "ellps": "WGS84",
+            "datum": "WGS84",
+            "units": "m"}
