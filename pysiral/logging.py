f7d40c526cb0bba5ecee4a31461ab81a492b3c84
Stefan Hendricks <stefan.hendricks@awi.de>
2016-04-23 17:12:35 +0200

    [logging] added a logging class with provides logging functionality to all inheriting classes

--- pysiral/logging.py
+++ pysiral/logging.py
@@ -13,3 +13,15 @@ def stdout_logger(name):
     StreamHandler(sys.stdout).push_application()
     log = Logger(name)
     return log
+
+
+class DefaultLoggingClass(object):
+
+    def __init__(self, name):
+        self.log = stdout_logger(name)
+
+    def info(self, *args, **kwargs):
+        self.log.info(*args, **kwargs)
+
+    def debug(self, *args, **kwargs):
+        self.log.debug(*args, **kwargs)
