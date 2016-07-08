84f6ca93f650ebfed2e284515ae68e4fc2f74e1f
Stefan Hendricks <stefan.hendricks@awi.de>
2016-07-08 17:47:20 +0200

    [config] Added TimeRangeRequest class for improved handling of selection of time periods for processing

--- pysiral/config.py
+++ pysiral/config.py
@@ -15,6 +15,7 @@ Created on Mon Jul 06 10:38:41 2015
 @author: Stefan
 """
 
+from pysiral.errorhandler import ErrorStatus
 import os
 import yaml
 from treedict import TreeDict
@@ -117,6 +118,176 @@ class RadarModes(object):
     @property
     def num(self):
         return len(self.flag_dict.keys())
+
+
+class TimeRangeRequest(object):
+
+    _PERIODS = ["monthly"]
+
+    def __init__(self):
+        self._start_dt = None
+        self._stop_dt = None
+        self._period = self._default_period
+        self._exclude_month = []
+        self.error = ErrorStatus()
+
+    def __repr__(self):
+        output = "TimeRangeRequest object:\n"
+        for field in ["_start_dt", "_stop_dt", "_period", "_exclude_month"]:
+            output += "%12s: %s" % (field, getattr(self, field))
+            output += "\n"
+        return output
+
+    def set_range(self, start_date, stop_date):
+
+        # Decode start and stop time definition
+
+        # Check if datetime objects
+        valid_start, valid_stop = False, False
+        if isinstance(start_date, datetime):
+            self._start_dt = start_date
+            valid_start = True
+        if isinstance(stop_date, datetime):
+            self._stopt_dt = stop_date
+            valid_stop = True
+
+        if valid_start and valid_stop:
+            return
+
+        # Check and decode integer lists
+        if isinstance(start_date, list):
+            if all(isinstance(item, int) for item in start_date):
+                self._start_dt = self._decode_int_list(start_date, "start")
+            else:
+                error_message = "invalid start time (not integer list)"
+                self.error.append(self.__class__.__name__, error_message)
+
+        if isinstance(stop_date, list):
+            if all(isinstance(item, int) for item in stop_date):
+                self._stop_dt = self._decode_int_list(stop_date, "stop")
+            else:
+                error_message = "invalid stop time (non integer list)"
+                self.error.append(self.__class__.__name__, error_message)
+
+    def clip_to_range(self, range_start, range_stop):
+
+        if self._start_dt < range_start and self._stop_dt > range_start:
+            self._start_dt = range_start
+        elif self._start_dt < range_start and self._stop_dt < range_start:
+            self._start_dt = None
+            self._stop_dt = None
+
+        if self._stop_dt > range_stop and self._start_dt < range_stop:
+            self._stop_dt = range_stop
+        elif self._stop_dt > range_stop and self._start_dt > range_stop:
+            self._start_dt = None
+            self._stop_dt = None
+
+    def set_period(self, period):
+        if period in self._PERIODS:
+            self._period = period
+        else:
+            raise ValueError("Invalid TimeRangeRequest period: %s" % period)
+
+    def raise_on_error(self):
+        if self.error.status:
+            print self.error.message
+            sys.exit(1)
+
+    def _decode_int_list(self, int_list, start_or_stop):
+
+        # XXX: Currently only yyyy mm [dd] (day is optional) are allowed
+        n_entries = len(int_list)
+        if n_entries < 2 or n_entries > 3:
+            error_message = "%s date integer list must be yyyy mm [dd]"
+            self.error.append(self.__class__.__name__, error_message)
+            return None
+
+        # Set the day
+        day = 1 if n_entries == 2 else int_list[2]
+
+        # Set the datetime object (as if would be start date)
+        # Raise error and return none if unsuccessful
+        try:
+            dt = datetime(int_list[0], int_list[1], day)
+        except:
+            error_message = "cannot convert integer list to datetime: %s" % (
+                str(int_list))
+            self.error.append(self.__class__.__name__, error_message)
+            return None
+
+        # if stop time: add one period
+        if start_or_stop == "stop":
+            if n_entries == 2:
+                extra_period = relativedelta(months=1, seconds=-1)
+            else:
+                extra_period = relativedelta(days=1, seconds=-1)
+            dt = dt + extra_period
+
+        return dt
+
+    @property
+    def _default_period(self):
+        return self._PERIODS[0]
+
+    @property
+    def start_dt(self):
+        return self._start_dt
+
+    @property
+    def stop_dt(self):
+        return self._stop_dt
+
+
+class DefaultCommandLineArguments(object):
+
+    def __init__(self):
+
+        config = ConfigInfo()
+
+        self._args = {
+
+            # Mission id
+            "mission": {
+                "action": 'store',
+                "dest": 'mission_id',
+                "choices": config.mission_ids,
+                "required": True,
+                "help": "pysiral recognized mission id"},
+
+            # Default date parameter
+            "date": {
+                "action": "store",
+                "dest": "stop_date",
+                "nargs": "+",
+                "type": int,
+                "required": False,
+                "help": 'list as year and month and day (optional)'},
+
+            # Default date parameter
+            "hemisphere": {
+                "action": "store",
+                "dest": "hemisphere",
+                "choices": ["global", "north", "south"],
+                "default": "global",
+                "required": False,
+                "help": 'hemisphere flag for processing)'},
+
+            # List of month to exclude from monthly iterations
+            "exclude-month": {
+                "action": "store",
+                "dest": "exclude_month",
+                "nargs": "+",
+                "type": int,
+                "required": False,
+                "default": None,
+                "help": 'list of months to be excluded from processing'}}
+
+    def get_argparse_dict(self, name, destination, required):
+        options = self._args[name]
+        options["dest"] = destination
+        options["required"] = required
+        return options
 
 
 def get_yaml_config(filename, output="treedict"):
