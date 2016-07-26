1aaffdecfbbf99ef21c5ae42f1e2904d33ad4447
Stefan Hendricks <stefan.hendricks@awi.de>
2016-07-26 17:51:11 +0200

    minor edits

--- sandbox/test_write_netcdf_config.py
+++ sandbox/test_write_netcdf_config.py
@@ -7,7 +7,6 @@ Created on Mon Jul 25 17:10:04 2016
 
 from pysiral.config import ConfigInfo, get_yaml_config
 
-
 from netCDF4 import Dataset
 import os
 
@@ -17,9 +16,6 @@ config = ConfigInfo()
 setting_file = os.path.join(
     config.pysiral_local_path, "settings", "l2", "cryosat2_north_awi.yaml")
 setting = get_yaml_config(setting_file)
-
-
-
 
 rootgrp = Dataset(r"test_write_netcdf_config.nc", "w")
 for item in setting.iterkeys():
