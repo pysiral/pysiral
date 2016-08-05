5e0c3857d2fb6fc96d5d1ab7e13cfade75f25b55
Stefan Hendricks <stefan.hendricks@awi.de>
2016-08-05 17:59:43 +0200

    [sentinel3a] code clean up of Sentinel-3a dataset parser

--- pysiral/sentinel3/sral_l1b.py
+++ pysiral/sentinel3/sral_l1b.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from pysiral.sentinel3.iotools import get_sentinel3_sral_l1_from_l2
-from pysiral.errorhandler import FileIOErrorHandler
 from pysiral.path import folder_from_filename
 
 import numpy as np
@@ -10,46 +8,35 @@ import os
 
 class Sentinel3SRALL1b(object):
 
-    def __init__(self, raise_on_error=False):
+    def __init__(self):
 
         # Error Handling
-        self._init_error_handling(raise_on_error)
         self.product_info = Sentinel3SRALProductInfo()
-        self._radar_mode = "sar"
         self._filename = None
         self.n_records = 0
-        self.range_bin_width = 0.234212857813 * 2.
-        self.nominal_tracking_bin = 60
-        self._xml_header_file = "xfdumanifest.xml"
-        self._xml_metadata_object_index = {
-            "processing": 0,
-            "acquisitionPeriod": 1,
-            "platform": 2,
-            "generalProductInformation": 3,
-            "measurementOrbitReference": 4,
-            "measurementQualityInformation": 5,
-            "measurementFrameSet": 6,
-            "sralProductInformation": 7}
 
     def parse(self):
-        self._parse_xml_header()
         self._parse_measurement_nc()
 
-    def _parse_xml_header(self):
+    def parse_xml_header(self, settings):
         """
         Parse the Sentinel-3 XML header file and extract key attributes
         for filtering
         """
 
-        filename_header = os.path.join(
-            folder_from_filename(self.filename), self._xml_header_file)
+        # Retrieve header information from mission settings
+        xml_header_file = settings.xml_header_file
+        xml_metadata_object_index = settings.xml_metadata_object_index
+
+        dataset_folder = folder_from_filename(self.filename)
+        filename_header = os.path.join(dataset_folder, xml_header_file)
         self._xmlh = parse_sentinel3_l1b_xml_header(filename_header)
 
         # Extract Metadata
         metadata = self._xmlh["metadataSection"]["metadataObject"]
 
         # Extract Product Info
-        index = self._xml_metadata_object_index["sralProductInformation"]
+        index = xml_metadata_object_index["sralProductInformation"]
         sral_product_info = metadata[index]["metadataWrap"]["xmlData"]
         sral_product_info = sral_product_info["sralProductInformation"]
 
@@ -60,8 +47,6 @@ class Sentinel3SRALL1b(object):
         open_ocean_percentage = sral_product_info["sral:openOceanPercentage"]
         self.product_info.open_ocean_percentage = float(open_ocean_percentage)
 
-        # print "sar_mode_percentage = %.1f" % int(sar_mode_percentage)
-
     def _parse_measurement_nc(self):
 
         from pysiral.iotools import ReadNC
@@ -70,48 +55,12 @@ class Sentinel3SRALL1b(object):
         # Read the L2 netCDF file
         self.nc = ReadNC(self.filename)
 
-        # Additionally read the L1 netCDF file for additional
-        # waveform and stack parameters
-        # L1 filename cannot be directly derived from L2 filename, thus
-        # it has to be estimated and looked for potential matches
-        # -> if l1nc_filename is None, corresponding L1 file does not exist
-#        l1nc_filename = get_sentinel3_sral_l1_from_l2(self.filename)
-#        if l1nc_filename is not None:
-#
-#            self.l1nc = ReadNC(l1nc_filename)
-#
-#            if len(self.l1nc.time_l1b_echo_sar_ku) == 0:
-#                print ". Warning - l1 ku data empty"
-#            else:
-#                # Verify time overlap between l1 and l2 data
-#                start_l1 = np.amin(self.l1nc.time_l1b_echo_sar_ku)
-#                end_l1 = np.amax(self.l1nc.time_l1b_echo_sar_ku)
-#
-#                start_l2 = np.amin(self.nc.time_20_ku)
-#                end_l2 = np.amax(self.nc.time_20_ku)
-#
-#                l1_l2_overlap = (start_l1 <= end_l2) and (end_l1 >= start_l2)
-#                if not l1_l2_overlap:
-#                    raise IOError("L1/L2 files - no overlap")
-#        else:
-#            print ". Warning - no l1 file match"
-
-#        for attribute in self.nc.attributes:
-#            print "attribute: %s = %s" % (
-#                attribute, str(getattr(self.nc, attribute)))
-#        for parameter in self.nc.parameters:
-#            print parameter, getattr(self.nc, parameter).shape
-
     def get_status(self):
         # XXX: Not much functionality here
         return False
 
     def post_processing(self):
-        """
-        The SGDR data structure needs to be streamlined, so that it
-        is easy to grab the relevant parameters as indiviual arrays
-        """
-        self._prepare_waveform_power_and_range()
+        pass
 
     @property
     def filename(self):
@@ -121,19 +70,13 @@ class Sentinel3SRALL1b(object):
     def filename(self, filename):
         """ Save and validate filenames for header and product file """
         # Test if valid file first
-        self._error.file_undefined = not os.path.isfile(filename)
-        if self._error.file_undefined:
+        if not os.path.isfile(filename):
             return
         self._filename = filename
 
     @property
     def radar_mode(self):
         return self._radar_mode
-
-    def _init_error_handling(self, raise_on_error):
-        self._error = FileIOErrorHandler()
-        self._error.raise_on_error = raise_on_error
-        self._error.file_undefined = True
 
     def _prepare_waveform_power_and_range(self):
         """
@@ -143,16 +86,17 @@ class Sentinel3SRALL1b(object):
 
         # self.wfm_power = self.nc.waveform_20_ku
         self.wfm_power = self.nc.i2q2_meas_ku_l1b_echo_sar_ku
-        n_records, n_range_bins = np.shape(self.wfm_power)
+
+        n_records, n_range_bins = shape = self.wfm_power.shape
         self.n_records = n_records
+
         # Get the window delay
         # "The tracker_range_20hz is the range measured by the onboard tracker
         #  as the window delay, corrected for instrumental effects and
         #  CoG offset"
         tracker_range_20hz = self.nc.range_ku_l1b_echo_sar_ku
 
-        self.wfm_range = np.ndarray(shape=self.wfm_power.shape,
-                                    dtype=np.float32)
+        self.wfm_range = np.ndarray(shape=shape, dtype=np.float32)
         range_bin_index = np.arange(n_range_bins)
         for record in np.arange(n_records):
             self.wfm_range[record, :] = tracker_range_20hz[record] + \
