88c4b28ec6f86a334c23726d5e0c30029a6a0d52
Stefan Hendricks <stefan.hendricks@awi.de>
2016-11-17 19:30:57 +0100

    [frb] added error handling and prototype uncertainty handling to freeboard module

--- pysiral/frb.py
+++ pysiral/frb.py
@@ -6,19 +6,22 @@ Created on Mon Apr 25 17:15:39 2016
 """
 
 from pysiral.config import options_from_dictionary
+from pysiral.errorhandler import ErrorStatus
+
+import numpy as np
 
 
 class L2FreeboardAlgorithmBaseClass(object):
 
     def __init__(self):
-        pass
+        self.error = ErrorStatus()
 
     def set_options(self, **opt_dict):
         self._options = options_from_dictionary(**opt_dict)
 
     def get_freeboard(self, l2):
-        freeboard, msg = self._get_freeboard(l2)
-        return freeboard, msg
+        freeboard, freeboard_uncertainty = self._get_freeboard(l2)
+        return freeboard, freeboard_uncertainty
 
 
 class SnowGeometricCorrection(L2FreeboardAlgorithmBaseClass):
@@ -29,15 +32,23 @@ class SnowGeometricCorrection(L2FreeboardAlgorithmBaseClass):
         super(SnowGeometricCorrection, self).__init__()
 
     def _get_freeboard(self, l2):
-        import numpy as np
+        """ Compute the freeboard and its uncertainty """
+
+        # Init parameter arrays
+        shape = l2.afrb.shape
+        freeboard = np.full(shape, np.nan, dtype=np.float32)
+        freeboard_uncertainty = np.full(shape, np.nan, dtype=np.float32)
+
+        # Compute the snow wave speed correction factor
         correction_factor = self._options.vacuum_light_speed_reduction
         geometric_correction = correction_factor * l2.snow_depth
-        sea_ice_only = l2.surface_type.sea_ice.indices
-        freeboard = np.ndarray(
-            shape=(l2.afrb.shape), dtype=np.float32) * np.nan
-        freeboard[sea_ice_only] = l2.afrb[sea_ice_only] + \
-            geometric_correction[sea_ice_only]
-        return freeboard, ""
+
+        # Compute freeboard only for sea ice waveforms
+        is_ice = l2.surface_type.sea_ice.indices
+        freeboard[is_ice] = l2.afrb[is_ice] + geometric_correction[is_ice]
+        freeboard_uncertainty[is_ice] = 0.0
+
+        return freeboard, freeboard_uncertainty
 
 
 def get_frb_algorithm(name):
