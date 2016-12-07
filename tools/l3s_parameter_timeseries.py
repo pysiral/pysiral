30debcc90114369bab6a145565a4f898c92f0831
Stefan Hendricks <stefan.hendricks@awi.de>
2016-12-07 15:16:08 +0100

    [tools] updated l3s parameter time series plotter

--- tools/l3s_parameter_timeseries.py
+++ tools/l3s_parameter_timeseries.py
@@ -10,12 +10,14 @@ from pysiral.logging import DefaultLoggingClass
 from pysiral.iotools import NCMaskedGridData
 from pysiral.path import validate_directory
 from pysiral.visualization.mapstyle import get_custom_font
+from pysiral.visualization.mapstyle import GridMapPaperStyle
+from pysiral.maptools import get_landcoastlines
 
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.dates import date2num
 from matplotlib.patches import Rectangle
-from matplotlib.collections import PatchCollection
+from mpl_toolkits.basemap import Basemap
 
 from datetime import datetime
 import argparse
@@ -30,6 +32,7 @@ class L3sParameterTimeSeries(DefaultLoggingClass):
         self.latlonbox = LatLonBox(area="world")
         self.output_folder = "auto"
         self.parameter_list = []
+        self.l4roimap = Level4ROIMap()
         self.l3s_stack = L3sSourceList()
         self.l4plotdef = Level4ParameterPlotSettings()
 
@@ -51,7 +54,16 @@ class L3sParameterTimeSeries(DefaultLoggingClass):
         # Update plot settings
         self.l4plotdef.update(labels=self.args.plot_label,
                               annotation=self.args.plot_annotation,
+                              custom_plot_range=self.args.plot_range,
                               export_map=self.args.export_map)
+
+        # Update map plot
+        self.l4roimap.is_active = self.args.export_map
+        self.l4roimap.annotation = self.args.plot_annotation
+        if self.args.roi_name is not None:
+            self.l4roimap.region_name = self.args.roi_name
+        else:
+            self.l4roimap.region_name = "global"
 
         # Set parameter_list
         self.parameter_list = self.args.parameter_list
@@ -87,6 +99,11 @@ class L3sParameterTimeSeries(DefaultLoggingClass):
             l4par_plot = Level4ParameterPlot(self.l4plotdef)
             l4par_plot.set_parameter_data(l4par)
             l4par_plot.export(self.output_folder)
+
+        # Export one map
+        if self.l4roimap.is_active:
+            self.l4roimap.set_roi(self.latlonbox)
+            self.l4roimap.export(self.output_folder)
 
     @property
     def arg_parser(self):
@@ -124,6 +141,10 @@ class L3sParameterTimeSeries(DefaultLoggingClass):
         parser.add_argument('-parameter', action='store',
                             dest='parameter_list', nargs='+', required=True,
                             help='list of parameter')
+
+        parser.add_argument('-plot-range', action='store',
+                            dest="plot_range", nargs=4, type=int,
+                            help='plot range: yyyy mm till yyyy mm')
 
         return parser
 
@@ -331,12 +352,30 @@ class LatLonBox(DefaultLoggingClass):
                                       lat <= self.latbounds[1])
         return np.logical_and(in_lon_range, in_lat_range)
 
+    def get_boundary_line(self, n_points_per_line=100):
+        lons = np.array([])
+        lats = np.array([])
+        lon_indices = [(0, 1,), (1, 1), (1, 0), (0, 0)]
+        lat_indices = [(0, 0), (0, 1), (1, 1), (1, 0)]
+        for i in np.arange(4):
+            lon_index, lat_index = lon_indices[i], lat_indices[i]
+            lon_line = np.linspace(self.lonbounds[lon_index[0]],
+                                   self.lonbounds[lon_index[1]],
+                                   n_points_per_line)
+            lat_line = np.linspace(self.latbounds[lat_index[0]],
+                                   self.latbounds[lat_index[1]],
+                                   n_points_per_line)
+            lons = np.append(lons, lon_line)
+            lats = np.append(lats, lat_line)
+        return lons, lats
+
     @property
     def full_name(self):
         return self._area_name
 
 
 class Level4ParameterPlotSettings(DefaultLoggingClass):
+    """ Contains all settings for the plot """
 
     def __init__(self):
         class_name = self.__class__.__name__
@@ -346,15 +385,98 @@ class Level4ParameterPlotSettings(DefaultLoggingClass):
         self.plot_range = None
         self.export_map = False
         self.region_name = None
+        self.custom_plot_range = None
 
     def update(self, **kwargs):
-        pass
+        for key in kwargs.keys():
+            setattr(self, key, kwargs[key])
 
     def set_region_name(self, region_name):
         self.region_name = region_name
 
     def set_parameter_name(self, parameter_name):
         self.parameter_name = parameter_name
+
+    def get_boxplotprops(self, mission_index):
+        color = self.mission_colors[mission_index]
+        boxplot_props = dict(
+            flierprops=dict(marker=',', color=color, markersize=1),
+            boxprops=dict(color=color, linewidth=1),
+            whiskerprops=dict(linestyle="-", color=color, linewidth=0.75),
+            capprops=dict(linestyle="-", color=color, linewidth=0.75),
+            meanprops=dict(zorder=101, mec="1.0", markerfacecolor=color,
+                           linewidth=1),
+            medianprops=dict(linewidth=0), showmeans=True, widths=5,
+            showfliers=False)
+        return boxplot_props
+
+    def get_boxfillprops(self, mission_index):
+        return dict(ec="none", fc=self.mission_colors[mission_index],
+                    zorder=100)
+
+    def get_labelfontprops(self, mission_index):
+        return dict(color=self.mission_colors[mission_index],
+                    fontproperties=get_custom_font(fontsize=16, awi_font=True))
+
+    @property
+    def output_filename(self):
+        # Simplify region name
+        regstr = self.region_name.lower()
+        regstr = regstr.replace(" ", "_")
+        # Simplify annotation
+        if self.annotation is None:
+            annstr = ""
+        else:
+            annstr = "_"+self.annotation.lower()
+            annstr = annstr.replace(" ", "_")
+        output_filename = "l4_%s_%s%s.png" % (self.parameter_name, regstr,
+                                              annstr)
+        return output_filename
+
+    @property
+    def savefigprops(self):
+        return dict(dpi=600)
+
+    @property
+    def yearfontprops(self):
+        return dict(color="#4b4b4d",
+                    fontproperties=get_custom_font(fontsize=12, awi_font=True))
+
+    def get_monthfontprops(self, xlim):
+        # Get font size
+        years = np.floor((xlim[1]-xlim[0])/365.)
+        if years <= 2:
+            fs = 11
+        if years >= 5:
+            fs = 5
+        else:
+            fs = 8
+        return dict(color="#4b4b4d",
+                    fontproperties=get_custom_font(fontsize=fs, awi_font=True))
+
+    @property
+    def yearboxprops(self):
+        return dict(ec="#4b4b4d", fc="0.94", lw=0.05)
+
+    @property
+    def parlabelfontprops(self):
+        return dict(color="#4b4b4d",
+                    fontproperties=get_custom_font(fontsize=12, awi_font=True))
+
+    @property
+    def mission_colors(self):
+        return ["#00ace5", "#003e6e"]
+
+    @property
+    def custom_plot_xlim(self):
+        if self.custom_plot_range is None:
+            xlim = None
+        else:
+            rng = self.custom_plot_range
+            startdt = datetime(rng[0], rng[1], 1)
+            stopdt = datetime(rng[2], rng[3], 1)
+            xlim = ([date2num(startdt), date2num(stopdt)])
+        return xlim
 
 
 class Level4ParameterPlot(DefaultLoggingClass):
@@ -370,6 +492,8 @@ class Level4ParameterPlot(DefaultLoggingClass):
     def export(self, output_folder):
         self._init_figure()
         self._create_plot()
+        self._set_plot_ranges()
+        self._set_plot_style()
         self._export(output_folder)
 
     def _init_figure(self):
@@ -379,101 +503,167 @@ class Level4ParameterPlot(DefaultLoggingClass):
         self.ax.set_axis_bgcolor('0.98')
 
     def _create_plot(self):
+        """ Boxplots and Mission labels """
+
+        # Get mission info (for full mission names)
         pysiral_config = ConfigInfo()
-        colors = ["#00ace5", "#003e6e"]
+
+        # Loop over missions
         for i, mission in enumerate(self.l4.missions):
 
-            flierprops = dict(marker=',', color=colors[i], markersize=1)
-            boxprops = dict(color=colors[i], linewidth=1)
-            whiskerprops = dict(linestyle="-", color=colors[i], linewidth=0.75)
-            capprops = whiskerprops
-            meanprops = dict(zorder=101, mec="1.0", markerfacecolor=colors[i])
-            medianprops = dict(linewidth=0)
-
+            # First draw the boxplots
+            boxplotprops = self.plotdef.get_boxplotprops(i)
             data, dates = self.l4.get_boxplot_data(mission)
-            bp = plt.boxplot(data, positions=dates,
-                             flierprops=flierprops, whiskerprops=whiskerprops,
-                             capprops=capprops, boxprops=boxprops,
-                             meanprops=meanprops, medianprops=medianprops,
-                             showmeans=True, widths=5,
-                             showfliers=False)
+            bp = plt.boxplot(data, positions=dates, **boxplotprops)
 
-            # Draw boxes as filled objects
+            # Fill boxes (for better visibility)
             for box in bp["boxes"]:
                 xdata, ydata = box.get_xdata(), box.get_ydata()
                 xmin, xmax = np.amin(xdata), np.amax(xdata)
                 ymin, ymax = np.amin(ydata), np.amax(ydata)
+                boxfillprops = self.plotdef.get_boxfillprops(i)
                 rect = Rectangle([xmin, ymin], xmax-xmin, ymax-ymin,
-                                 ec="none", fc=colors[i], zorder=100)
+                                 **boxfillprops)
                 self.ax.add_patch(rect)
 
-            fontprops = {
-                "color": colors[i],
-                "fontproperties": get_custom_font(fontsize=16, awi_font=True)}
-
+            # Add the mission label
             mission_info = pysiral_config.get_mission_info(mission)
             labelx = 0.05 + i*0.075
-            plt.annotate(mission_info.long_name, (labelx, 0.05),
+            plt.annotate(mission_info.long_name, (labelx, 0.03),
                          xycoords="figure fraction",
-                         **fontprops)
+                         **self.plotdef.get_labelfontprops(i))
 
-        # Force correct xticks
+    def _set_plot_ranges(self):
+
         xticks, xticklabels = self.l4.get_all_periods()
         self.ax.set_xticks(xticks)
         self.ax.set_xticklabels(xticklabels)
 
-        fontprops = {
-            "color": "#4b4b4d",
-            "fontproperties": get_custom_font(fontsize=12, awi_font=True)}
+        # Set plot ranges
+        if self.plotdef.custom_plot_xlim is None:
+            plt.xlim(self.l4.get_data_range(paddays=30))
+        else:
+            plt.xlim(self.plotdef.custom_plot_xlim)
 
-        patches = []
+        plt.ylim(-0.5, 4)
+
+    def _set_plot_style(self):
+        """ Axis Style """
+
+        # Add year rectangles for better visibility
         for year in self.l4.years:
             labelx = date2num(datetime(year, 7, 1))
             plt.annotate(str(year), (labelx, 3.8), xycoords="data",
-                         ha="center", **fontprops)
+                         ha="center", **self.plotdef.yearfontprops)
             if np.mod(year, 2) == 0:
                 continue
             xmin = date2num(datetime(year, 1, 1))
-            ymin = -0.5
-            width = 365
-            height = 4.5
-            rect = Rectangle([xmin, ymin], width, height, ec="none", fc="0.94")
+            ymin, width, height = -0.5, 365, 4.5
+            rect = Rectangle([xmin, ymin], width, height,
+                             **self.plotdef.yearboxprops)
             self.ax.add_patch(rect)
 
-        plt.xlim(self.l4.get_data_range(paddays=30))
-        plt.ylim(-0.5, 4)
-
+        # Remove missor axis
         spines_to_remove = ["top", "right"]
         for spine in spines_to_remove:
             self.ax.spines[spine].set_visible(False)
 
-        self.ax.yaxis.set_tick_params(direction='out')
-        self.ax.yaxis.set_ticks_position('left')
-        self.ax.spines["left"].set_position(("axes", -0.01*6./16.))
-
+        # x-axis (x-ticks are forced to remove shifts for multiple missions)
         self.ax.xaxis.set_tick_params(direction='out')
         self.ax.xaxis.set_ticks_position('bottom')
         self.ax.spines["bottom"].set_position(("axes", -0.01))
-
-        fontprops = {
-            "color": "#4b4b4d",
-            "fontproperties": get_custom_font(fontsize=5, awi_font=True)}
         cl = plt.getp(self.ax, 'xmajorticklabels')
-        plt.setp(cl, **fontprops)
+        plt.setp(cl, **self.plotdef.get_monthfontprops(self.ax.get_xlim()))
 
-        fontprops = {
-            "color": "#4b4b4d",
-            "fontproperties": get_custom_font(fontsize=12, awi_font=True)}
-
+        # y-axis
+        self.ax.set_ylabel("Sea Ice Thickness (meter)",
+                           **self.plotdef.parlabelfontprops)
+        self.ax.yaxis.set_tick_params(direction='out')
+        self.ax.yaxis.set_ticks_position('left')
+        self.ax.spines["left"].set_position(("axes", -0.01*6./16.))
         cl = plt.getp(self.ax, 'ymajorticklabels')
-        plt.setp(cl, **fontprops)
-        self.ax.set_ylabel("Sea Ice Thickness (meter)", **fontprops)
+        plt.setp(cl, **self.plotdef.parlabelfontprops)
 
     def _export(self, output_folder):
+        """ png export """
         validate_directory(output_folder)
-        filename = os.path.join(output_folder, "test.png")
-        plt.savefig(filename, dpi=600)
+        filename = os.path.join(output_folder, self.plotdef.output_filename)
+        plt.savefig(filename, **self.plotdef.savefigprops)
         plt.close(self.figure)
+
+
+class Level4ROIMap(object):
+
+    def __init__(self):
+        self.is_active = False
+        self.roi = None
+        self.annotation = None
+
+    def set_roi(self, roi):
+        self.roi = roi
+
+    def export(self, output_folder):
+
+        # get style from pysiral definition
+        style = GridMapPaperStyle()
+
+        # switch off interactive plotting
+        plt.ioff()
+        figure = plt.figure(**style.figure.keyw)
+
+        # Basemap settings
+        m = Basemap(**self.basemap_args)
+        m.drawmapboundary(**style.mapboundary.keyw)
+        m.fillcontinents(zorder=120, **style.continents.keyw)
+
+#        parallels = np.arange(0., 88., 2.)
+#        m.drawparallels(parallels, labels=[0, 0, 0, 0], fontsize=20, latmax=88,
+#                        linewidth=0.25, dashes=(None, None), color="#4b4b4b")
+#        meridians = np.arange(0., 360., 10.)
+#        m.drawmeridians(meridians, labels=[0, 0, 0, 0], fontsize=20, latmax=88,
+#                        linewidth=0.25, dashes=(None, None), color="#4b4b4b")
+
+        if style.coastlines.is_active:
+            coastlines = get_landcoastlines(m, **style.coastlines.keyw)
+            coastlines.set_zorder(120)
+            plt.gca().add_collection(coastlines)
+
+        lons, lats = self.roi.get_boundary_line()
+        x, y = m(lons, lats)
+        plt.plot(x, y, linewidth=4, color="#FF2400", zorder=400)
+#        plt.plot(x, y, linewidth=4, color="#4b4b4b", zorder=400)
+        # "#76FF7A"
+#        x = config.basemap_args["height"] / 2.
+#        plt.plot(np.array([-x, x, x, -x, -x])+size/2.,
+#                 np.array([-x, -x, x, x, -x])+size/2.,
+#                 linewidth=4, color="#4b4b4b", zorder=400)
+
+        # save the file
+#        mapfile = config.output_basename + "_overview_map.png"
+        filename = os.path.join(output_folder, self.output_filename)
+        plt.savefig(filename, dpi=150, facecolor=figure.get_facecolor(),
+                    bbox_inches="tight")
+        plt.close(figure)
+
+    @property
+    def basemap_args(self):
+        return dict(projection="laea", resolution="i",  width=6000000,
+                    height=6000000, lon_0=-45, lat_0=90)
+
+    @property
+    def output_filename(self):
+        # Simplify region name
+        regstr = self.region_name.lower()
+        regstr = regstr.replace(" ", "_")
+        # Simplify annotation
+        if self.annotation is None:
+            annstr = ""
+        else:
+            annstr = "_"+self.annotation.lower()
+            annstr = annstr.replace(" ", "_")
+        output_filename = "l4map_%s%s.png" % (regstr, annstr)
+        return output_filename
+
 
 
 if __name__ == "__main__":
