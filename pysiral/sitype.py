cbb952af9d38c6faa8ed9a1f374ef9c7bdda93de
Stefan Hendricks <stefan.hendricks@awi.de>
2017-09-25 21:39:12 +0200

    [sitype] Added sitype class for osi-saf sitype cdr products

--- pysiral/sitype.py
+++ pysiral/sitype.py
@@ -129,6 +129,131 @@ class OsiSafSIType(SITypeBaseClass):
         return sitype, sitype_uncertainty, self._msg
 
 
+class OsiSafSITypeCDR(SITypeBaseClass):
+    """ Class for reprocessed OSISAF sea ice type products (e.g. for C3S).
+    Needs to be merged into single OsiSafSitype class at some point """
+
+    def __init__(self):
+        super(OsiSafSITypeCDR, self).__init__()
+
+        self._data = None
+        self._current_date = [0, 0, 0]
+        self._requested_date = [-1, -1, -1]
+
+    @property
+    def year(self):
+        return "%04g" % self._requested_date[0]
+
+    @property
+    def month(self):
+        return "%02g" % self._requested_date[1]
+
+    @property
+    def day(self):
+        return "%02g" % self._requested_date[2]
+
+    def _get_along_track_sitype(self, l2):
+        self._get_requested_date(l2)
+        self._get_data(l2)
+        if self.error.status:
+            return None, self.error.message
+        sitype, uncertainty = self._get_sitype_track(l2)
+        return sitype, uncertainty, self._msg
+
+    def _get_requested_date(self, l2):
+        """ Use first timestamp as reference, date changes are ignored """
+        year = l2.track.timestamp[0].year
+        month = l2.track.timestamp[0].month
+        day = l2.track.timestamp[0].day
+        self._requested_date = [year, month, day]
+
+    def _get_data(self, l2):
+        """ Loads file from local repository only if needed """
+        if self._requested_date == self._current_date:
+            # Data already loaded, nothing to do
+            return
+
+        # construct filename
+        path = self._get_local_repository_filename(l2)
+
+        # Validation
+        if not os.path.isfile(path):
+            msg = "OsiSafSIType: File not found: %s " % path
+            self.error.add_error("auxdata_missing_sitype", msg)
+            return
+
+        # Read and prepare input data
+        self._data = ReadNC(path)
+        self._data.ice_type = np.flipud(self._data.ice_type[0, :, :])
+        self._data.uncertainty = np.flipud(self._data.uncertainty[0, :, :])
+
+        # Logging
+        self._msg = "OsiSafSIType: Loaded SIType file: %s" % path
+        self._current_date = self._requested_date
+
+    def _get_local_repository_filename(self, l2):
+        path = self._local_repository
+        for subfolder_tag in self._subfolders:
+            subfolder = getattr(self, subfolder_tag)
+            path = os.path.join(path, subfolder)
+        filename = self._filenaming.format(
+            year=self.year, month=self.month, day=self.day,
+            hemisphere_code=l2.hemisphere_code)
+        path = os.path.join(path, filename)
+        return path
+
+    def _get_sitype_track(self, l2):
+        """ Extract ice type and ice type uncertainty along the track """
+
+        # Convert grid/track coordinates to grid projection coordinates
+        kwargs = self._options[l2.hemisphere].projection
+        p = Proj(**kwargs)
+        x, y = p(self._data.lon, self._data.lat)
+        l2x, l2y = p(l2.track.longitude, l2.track.latitude)
+
+        # Convert track projection coordinates to image coordinates
+        # x: 0 < n_lines; y: 0 < n_cols
+        dim = self._options[l2.hemisphere].dimension
+        x_min = x[dim.n_lines-1, 0]
+        y_min = y[dim.n_lines-1, 0]
+        ix, iy = (l2x-x_min)/dim.dx, (l2y-y_min)/dim.dy
+
+        # Extract along track data from grid
+        sitype = ndimage.map_coordinates(
+            self._data.ice_type, [iy, ix], order=0)
+        uncertainty = ndimage.map_coordinates(
+            self._data.uncertainty, [iy, ix], order=0)
+
+        # Convert flags to myi fraction
+        translator = np.array([np.nan, np.nan, 0.0, 1.0, 0.5, np.nan])
+        fillvalues = np.where(sitype == -1)[0]
+        sitype[fillvalues] = 5
+        sitype = np.array([translator[value] for value in sitype])
+
+        # Uncertainty in product is in %
+        sitype_uncertainty = uncertainty / 100.
+
+#        import matplotlib.pyplot as plt
+#
+#        plt.figure(dpi=150)
+#        plt.imshow(self._data.ice_type, interpolation="none")
+#        plt.plot(ix, iy)
+#        plt.scatter(ix[0], iy[0])
+#
+#        plt.figure("projection coordinates")
+#        plt.plot(l2x, l2y)
+#
+#        plt.figure("sitype along track")
+#        plt.plot(sitype)
+#        plt.plot(sitype_uncertainty)
+#
+#        plt.show()
+#
+#        stop
+
+        return sitype, sitype_uncertainty
+
+
 class ICDCNasaTeam(SITypeBaseClass):
     """ MYI Fraction from NASA Team Algorithm (from ICDC UHH) """
 
