6fb5b5bedbc1391875b4539b9709509147444a04
Stefan Hendricks <stefan.hendricks@awi.de>
2018-02-12 11:14:31 +0100

    [config] Bump pysiral version

--- pysiral/config.py
+++ pysiral/config.py
@@ -33,8 +33,8 @@ from treedict import TreeDict
 
 import numpy as np
 
-PYSIRAL_VERSION = "0.5.0"
-PYSIRAL_VERSION_FILENAME = "050"
+PYSIRAL_VERSION = "0.6.0dev"
+PYSIRAL_VERSION_FILENAME = "060dev"
 HOSTNAME = socket.gethostname()
 
 SENSOR_NAME_DICT = {"ers1": "RA", "ers2": "RA", "envisat": "RA-2",
