afd8bcdf1b427052adeffab5441a257a2bcab76f
Stefan Hendricks <stefan.hendricks@awi.de>
2018-08-11 13:36:19 +0200

    [setup] Add cythonize to setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 
+from Cython.Build import cythonize
+from setuptools.extension import Extension
 from setuptools import setup, find_packages
+
+import numpy
+import os
 
 with open('README.rst') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
+
+extensions = [
+    Extension("pysiral.bnfunc.cytfmra", [os.path.join("pysiral", "bnfunc", "cytfmra.pyx")])]
 
 setup(
     name='pysiral',
@@ -19,4 +27,6 @@ setup(
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     scripts=['bin/pysiral-l1bpreproc', 'bin/pysiral-l2proc', 'bin/pysiral-l2preproc', 'bin/pysiral-l3proc'],
+    ext_modules = cythonize(extensions),
+    include_dirs = [numpy.get_include()]
 )
