0d50a12c45f8a0e6b8d230fb9bc94a754eb3a778
Stefan Hendricks <stefan.hendricks@awi.de>
2018-08-25 18:52:37 +0200

    [sic] update osisaf class with new auxdata parent class methods

--- pysiral/sic.py
+++ pysiral/sic.py
@@ -5,7 +5,7 @@ Created on Sun Apr 24 13:57:56 2016
 @author: Stefan
 """
 
-from pysiral.auxdata import AuxdataBaseClass
+from pysiral.auxdata import AuxdataBaseClass, GridTrackInterpol
 from pysiral.iotools import ReadNC
 
 import scipy.ndimage as ndimage
@@ -36,39 +36,69 @@ class OsiSafSIC(SICBaseClass):
         pass
 
     def _get_along_track_sic(self, l2):
-        self._msg = ""
+        """ Main entry point of the class """
+
+        # These properties are needed to construct the product path
+        self.start_time = l2.info.start_time
+        self.hemisphere_code = l2.hemisphere_code
+
+        # Set the requested date
         self.set_requested_date_from_l2(l2)
-        self._get_data(l2)
-        if not self.error.status:
-            sic = self._get_sic_track(l2)
-            return sic, self._msg
-        else:
+
+        # Update the external data
+        self.update_external_data()
+
+        # Check if error with file I/O
+        if self.error.status:
             return None, self._msg
 
-    def _get_data(self, l2):
-        """ Loads file from local repository only if needed """
-        if self._requested_date == self._current_date:
-            # Data already loaded, nothing to do
-            self._msg = self.__class__.__name__+": Daily grid already present"
-            return
-        path = self._get_local_repository_filename(l2)
+        # Get and return the track
+        sic = self._get_sic_track(l2)
 
-        # Validation
+
+        # All done, return
+        return sic, self._msg
+
+    def load_requested_auxdata(self):
+        """ Required subclass method: Load the data file necessary to satisfy condition for requested date"""
+
+        # Retrieve the file path for the requested date from a property of the auxdata parent class
+        path = self.requested_filepath
+
+        #  --- Validation ---
         if not os.path.isfile(path):
-            self._msg = "OsiSafSIC: File not found: %s " % path
+            self._msg = self.__class__.__name__+": File not found: %s " % path
             self.error.add_error("auxdata_missing_sic", self._msg)
             return
 
+        # --- Read the data ---
         self._data = ReadNC(path)
+
+        # --- Pre-process the data ---
+        # Remove time dimension
         self._data.ice_conc = self._data.ice_conc[0, :, :]
+
+        # No negative ice concentrations
         flagged = np.where(self._data.ice_conc < 0)
         self._data.ice_conc[flagged] = 0
-        # This step is important for calculation of image coordinates
-        self._data.ice_conc = np.flipud(self._data.ice_conc)
-        self._msg = "OsiSafSIC: Loaded SIC file: %s" % path
-        self._current_date = self._requested_date
 
-    def _get_local_repository_filename(self, l2):
+        self._msg = "OsiSafSIC: Loaded SIC file: %s" % path
+
+    def _get_sic_track(self, l2):
+        """ Simple extraction along trajectory"""
+
+        # Extract from grid
+        griddef = self._options[l2.hemisphere]
+        grid_lons, grid_lats = self._data.lon, self._data.lat
+        grid2track = GridTrackInterpol(l2.track.longitude, l2.track.latitude, grid_lons, grid_lats, griddef)
+        sic = grid2track.get_from_grid_variable(self._data.ice_conc, flipud=True)
+
+        return sic
+
+    @property
+    def requested_filepath(self):
+        """ Note: this overwrites the property in the super class due to some
+        peculiarities with the filenaming (auto product changes etc) """
 
         # Unique to this class is the possiblity to auto merge
         # products. The current implementation supports only two products
@@ -77,7 +107,7 @@ class OsiSafSIC(SICBaseClass):
         # The path needs to be completed if two products shall be used
         if "auto_product_change" in self.options:
             opt = self.options.auto_product_change
-            product_index = int(l2.info.start_time > opt.date_product_change)
+            product_index = int(self.start_time > opt.date_product_change)
             product_def = opt.osisaf_product_def[product_index]
             path = os.path.join(path, product_def["subfolder"])
             self.set_filenaming(product_def["filenaming"])
@@ -89,46 +119,9 @@ class OsiSafSIC(SICBaseClass):
 
         filename = self._filenaming.format(
             year=self.year, month=self.month, day=self.day,
-            hemisphere_code=l2.hemisphere_code)
+            hemisphere_code=self.hemisphere_code)
         path = os.path.join(path, filename)
         return path
-
-    def _get_sic_track(self, l2):
-
-        # Convert grid/track coordinates to grid projection coordinates
-        kwargs = self._options[l2.hemisphere].projection
-        p = Proj(**kwargs)
-        x, y = p(self._data.lon, self._data.lat)
-        l2x, l2y = p(l2.track.longitude, l2.track.latitude)
-
-        # Convert track projection coordinates to image coordinates
-        # x: 0 < n_lines; y: 0 < n_cols
-        dim = self._options[l2.hemisphere].dimension
-        x_min = x[dim.n_lines-1, 0]-(0.5*dim.dx)
-        y_min = y[dim.n_lines-1, 0]-(0.5*dim.dy)
-        ix, iy = (l2x-x_min)/dim.dx, (l2y-y_min)/dim.dy
-
-        # Extract along track data from grid
-        sic = ndimage.map_coordinates(self._data.ice_conc, [iy, ix], order=0)
-
-#        import matplotlib.pyplot as plt
-#
-#        plt.figure(dpi=150)
-#        plt.imshow(self._data.ice_conc, interpolation="none")
-#        plt.plot(ix, iy)
-#        plt.scatter(ix[0], iy[0])
-#
-#        plt.figure("projection coordinates")
-#        plt.plot(l2x, l2y)
-#
-#        plt.figure("sic along track")
-#        plt.plot(sic)
-#        plt.show()
-#
-#        stop
-
-        return sic
-
 
 class IfremerSIC(SICBaseClass):
 
