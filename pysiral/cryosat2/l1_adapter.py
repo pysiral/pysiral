1f70bf0c6018e7d89a114b3f7f39ff732cbbf436
Stefan Hendricks <stefan.hendricks@awi.de>
2018-11-15 17:06:26 +0100

    [cryosat2] Initial version of baseline-d l1 adaptor

--- pysiral/cryosat2/l1_adapter.py
+++ pysiral/cryosat2/l1_adapter.py
@@ -1,13 +1,44 @@
 
+import os
 from pysiral.errorhandler import ErrorStatus
 from pysiral.logging import DefaultLoggingClass
 
 
 class ESAPDSBaselineD(DefaultLoggingClass):
 
-    def __init__(self, cfg):
+    def __init__(self, cfg, raise_on_error=False):
+
         cls_name = self.__class__.__name__
         super(ESAPDSBaselineD, self).__init__(cls_name)
         self.error = ErrorStatus(caller_id=cls_name)
 
+        # Store arguments
+        self.raise_on_error = raise_on_error
         self.cfg = cfg
+
+        # Init main class variables
+        self.nc = None
+
+    def get_l1(self, filepath):
+        """
+        Main entry point to the CryoSat-2 Baseline-D Input Adapter
+        :param filepath:
+        :return:
+        """
+
+        # Input Validation
+        if not os.path.isfile(filepath):
+            msg = "Not a valid file: %s" % filepath
+            self.log.warning(msg)
+            self.error.add_error("invalid-filepath", msg)
+            return self.empty
+
+        # Parse the input file
+        self._read_input_netcdf()
+
+
+
+
+    @property
+    def empty(self):
+        return None
