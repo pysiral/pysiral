da1a47add97354649f7788096f34fe6a03102966
Stefan Hendricks <stefan.hendricks@awi.de>
2018-11-16 17:16:35 +0100

    [cryosat] improved the baseline-d l1 adapter (work in progress)

--- pysiral/cryosat2/l1_adapter.py
+++ pysiral/cryosat2/l1_adapter.py
@@ -1,7 +1,17 @@
 
 import os
+import sys
+import xarray
+import numpy as np
+
+from pysiral import __version__ as pysiral_version
+from pysiral.clocks import StopWatch
+from pysiral.cryosat2 import cs2_procstage2timeliness
 from pysiral.errorhandler import ErrorStatus
+from pysiral.helper import parse_datetime_str
+from pysiral.l1bdata import Level1bData
 from pysiral.logging import DefaultLoggingClass
+from pysiral.path import filename_from_path
 
 
 class ESAPDSBaselineD(DefaultLoggingClass):
@@ -19,12 +29,18 @@ class ESAPDSBaselineD(DefaultLoggingClass):
         # Init main class variables
         self.nc = None
 
-    def get_l1(self, filepath):
+    def get_l1(self, filepath, polar_ocean_check=None):
         """
         Main entry point to the CryoSat-2 Baseline-D Input Adapter
         :param filepath:
         :return:
         """
+
+        # Save filepath
+        self.filepath = filepath
+
+        # Create an empty Level-1 data object
+        self.l1 = Level1bData()
 
         # Input Validation
         if not os.path.isfile(filepath):
@@ -34,10 +50,60 @@ class ESAPDSBaselineD(DefaultLoggingClass):
             return self.empty
 
         # Parse the input file
-        self._read_input_netcdf()
+        self._read_input_netcdf(filepath, attributes_only=True)
+
+        # Get metadata
+        self._get_input_file_metadata()
+
+        if polar_ocean_check is not None:
+            has_polar_ocean_data = polar_ocean_check.has_polar_ocean_segments(self.l1.info)
+            if not has_polar_ocean_data:
+                return None
+
+    def _read_input_netcdf(self, filepath, attributes_only=False):
+        timer = StopWatch()
+        timer.start()
+        self.nc = xarray.open_dataset(filepath)
+        timer.stop()
+        self.log.info("Read netCDF file in %s" % timer.get_duration())
 
 
+    def _get_input_file_metadata(self):
+        """ Fill the product info """
 
+        # Short cuts
+        metadata =  self.nc.attrs
+        info = self.l1.info
+
+        # Processing environment metadata
+        info.set_attribute("pysiral_version", "pysiral_version")
+
+        # General product metadata
+        info.set_attribute("mission", "cryosat2")
+        info.set_attribute("mission_sensor", "siral")
+        info.set_attribute("mission_data_version", "D")
+        info.set_attribute("orbit", metadata["abs_orbit_start"])
+        info.set_attribute("cycle", metadata["cycle_number"])
+        info.set_attribute("mission_data_source", filename_from_path(self.filepath))
+        info.set_attribute("timeliness", cs2_procstage2timeliness(metadata["processing_stage"]))
+
+        # Time-Orbit Metadata
+        lats = [float(metadata["first_record_lat"])*1e-6, float(metadata["last_record_lat"])*1e-6]
+        lons = [float(metadata["first_record_lon"])*1e-6, float(metadata["last_record_lon"])*1e-6]
+        info.set_attribute("start_time", parse_datetime_str(metadata["first_record_time"][4:]))   # TAI=....
+        info.set_attribute("stop_time", parse_datetime_str(metadata["last_record_time"][4:]))     # TAI=....
+        info.set_attribute("lat_min", np.amin(lats))
+        info.set_attribute("lat_max", np.amax(lats))
+        info.set_attribute("lon_min", np.amin(lons))
+        info.set_attribute("lon_max", np.amax(lons))
+
+        # Product Content Metadata
+        for mode in ["sar", "sin", "lrm"]:
+            percent_value = 0.0
+            if metadata["sir_op_mode"].strip().lower() == mode:
+                percent_value = 100.
+            info.set_attribute("{}_mode_percent".format(mode), percent_value)
+        info.set_attribute("open_ocean_percent", float(metadata["open_ocean_percent"])*0.01)
 
     @property
     def empty(self):
