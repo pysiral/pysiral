f6c71aa40e45bcc8c8244ff06b31b049c75b4db9
Stefan Hendricks <stefan.hendricks@awi.de>
2019-08-09 18:18:44 +0200

    [cryosat2] bugfix: keys needed to be lowercase

--- pysiral/cryosat2/__init__.py
+++ pysiral/cryosat2/__init__.py
@@ -14,6 +14,6 @@ def cs2_procstage2timeliness(processing_stage_str):
     :param processing_stage_str: A String from the CryoSat-2 product metadata
     :return: the 3-character pysiral timeliness code
     """
-    timeliness_dct = {"n": "nrt", "o": "ntc", "r": "rep", "l": "rep", "test": "tds", "OFFL": "rep",
-                      "NRT_": "nrt"}
+    timeliness_dct = {"n": "nrt", "o": "ntc", "r": "rep", "l": "rep", "test": "tds", "offl": "rep",
+                      "nrt_": "nrt"}
     return timeliness_dct.get(processing_stage_str.lower(), "ukn")
