fbc229d9ae741926b16af05eb7aec969e664ecda
Stefan Hendricks <stefan.hendricks@awi.de>
2019-08-25 16:09:26 +0200

    [pysiral] bump version and amend changelog for bugfix release

--- pysiral/config.py
+++ pysiral/config.py
@@ -37,8 +37,8 @@ from isodate import duration_isoformat
 
 
 # TODO: These have to be removed (see __version__ in __init__.py)
-PYSIRAL_VERSION = "0.7.2"
-PYSIRAL_VERSION_FILENAME = "072"
+PYSIRAL_VERSION = "0.7.2.1"
+PYSIRAL_VERSION_FILENAME = "0721"
 HOSTNAME = socket.gethostname()
 
 # TODO: These have to removed (better use info from mission_def)
