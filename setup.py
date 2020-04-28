f483de91f859f3c28855305cc4ca4405262fe027
sthendric <stefan.hendricks@googlemail.com>
2020-04-28 18:46:12 +0200

    [package] add other python version classifiers

--- setup.py
+++ setup.py
@@ -41,7 +41,10 @@ setup(
         'Topic :: Scientific/Engineering :: Physics',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     long_description=readme,
     author='Stefan Hendricks',
