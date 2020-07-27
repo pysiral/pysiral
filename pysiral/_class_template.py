ba40ccdd56e3c305b3de781c903bffeca40e3707
Stefan Hendricks <stefan.hendricks@googlemail.com>
2020-07-27 19:21:09 +0200

    [pysiral] add a little documentation

--- pysiral/_class_template.py
+++ pysiral/_class_template.py
@@ -5,7 +5,14 @@ from pysiral.errorhandler import ErrorStatus
 
 
 class DefaultLoggingClass(object):
+    """
+    Template for default pysiral class with logging/error handling capabilities
+    """
 
-    def __init__(self, cls_name):
+    def __init__(self, cls_name=None):
+        """
+        Init the class with a (loguru) logger and an ErrorStatus error handler
+        :param cls_name:
+        """
         self.log = logger
         self.error = ErrorStatus(cls_name)
