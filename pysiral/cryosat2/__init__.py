dbf772024dd77bd789396e4f41af5f372147c01c
Stefan Hendricks <stefan.hendricks@googlemail.com>
2020-08-05 11:47:55 +0200

    [cryosat2] bugfix: timeliness tag list did not include the long-term archive tag from the baseline-D reprocessing

--- pysiral/cryosat2/__init__.py
+++ pysiral/cryosat2/__init__.py
@@ -4,6 +4,8 @@
 plib A collection of python libraries
 """
 
+from loguru import logger
+
 __all__ = ["functions", "iotools", "cs2_procstage2timeliness"]
 
 
@@ -14,5 +16,17 @@ def cs2_procstage2timeliness(processing_stage_str):
     :param processing_stage_str: A String from the CryoSat-2 product metadata
     :return: the 3-character pysiral timeliness code
     """
-    timeliness_dct = {"n": "nrt", "o": "ntc", "r": "rep", "l": "rep", "test": "tds", "offl": "rep", "nrt_": "nrt"}
+
+    # Range of option found in CryoSat-2 L1 metadata:
+    timeliness_dct = {"n": "nrt", "nrt_": "nrt",                              # near-real time
+                      "o": "ntc",                                             # non-time critical
+                      "test": "tds",                                          # test data set
+                      "r": "rep", "l": "rep", "offl": "rep", "lta_": "rep"}   # reprocessed
+
+    # Raise a warning if not found since timeliness tag will end up in filename
+    if processing_stage_str.lower() not in timeliness_dct.keys():
+        msg = "Timeliness metadata tag ({}) not in range of expected values: [{}]"
+        msg = msg.format(processing_stage_str.lower(), ", ".join(timeliness_dct.keys()))
+        logger.warning(msg)
+
     return timeliness_dct.get(processing_stage_str.lower(), "ukn")
