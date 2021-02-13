168280dd033cc12963e2846e7012c23542cac1b1
Stefan Hendricks <stefan.hendricks@googlemail.com>
2021-02-13 16:02:05 +0100

    [dot] Add dynamic ocean topography module

---
+++ pysiral/dot.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Stefan Hendricks
+
+pysiral module for estimating dynamic ocean topography from existing sea-level anomaly data
+child classes of pysiral.l2proc.procsteps.Level2ProcessorStep.
+
+NOTES:
+
+    1. The convention of the pysiral Level-2 processor is to compute dynamic ocean topography as
+
+            dot = sla + mdt
+
+       Thus, the auxiliary data set mean dynamic topography (mdt) is a mandatory auxiliary data
+       set for the functionality of all classes in this module and the sla anomaly must be
+       computed first.
+
+"""
+
+__author__ = "Stefan Hendricks <stefan.hendricks@awi.de>"
+
+
+from pysiral.l2proc.procsteps import Level2ProcessorStep
+
+
+class DynamicOceanTopography(Level2ProcessorStep):
+    """
+    A Level-2 processor step that adds sea ice density and sea ice density uncertainty
+    to the L2 data object based on sea ice type classification
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(DynamicOceanTopography, self).__init__(*args, **kwargs)
+
+    def execute_procstep(self, l1b, l2):
+        """
+        Mandatory Level-2 processor method that will execute the processing step and modify the
+        L2 data object in-place.
+        This method adds the dynamic ocean topography fields (dot) to the l2 data object
+        sla values.
+        :param l1b:
+        :param l2:
+        :return:
+        """
+
+        # The dynamic ocean topography is either defined as
+        #    1. DOT = SSH - geoid
+        #    2. DOT = SLA + MDT
+        # Here we use the second variant
+        dot = l2.sla + l2.mdt
+        l2.dot.set_value(dot)
+        l2.dot.set_uncertainty(l2.sla.uncertainty)
+
+    @property
+    def l2_input_vars(self):
+        """
+        Mandatory property for Level2ProcessorStep children
+        :return: list (str)
+        """
+        return ["mdt", "sla"]
+
+    @property
+    def l2_output_vars(self):
+        """
+        Mandatory property for Level2ProcessorStep children
+        :return: list (str)
+        """
+        return ["dot"]
+
+    @property
+    def error_bit(self):
+        return self.error_flag_bit_dict["sla"]
+
+
+if __name__ == "__main__":
+    main()
