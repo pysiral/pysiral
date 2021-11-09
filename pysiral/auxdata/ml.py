ef6ba8ef056891a4a53539e3e137e41127c5d161
Stefan Hendricks <stefan.hendricks@awi.de>
2021-11-09 12:08:28 +0100

    [ml] proof of concept for determining the tfmra threshold for sea ice waveforms directly from trained model data

--- pysiral/auxdata/ml.py
+++ pysiral/auxdata/ml.py
@@ -37,6 +37,7 @@ import re
 from pathlib import Path
 from typing import Iterable, Any, Union
 
+import numpy as np
 import xgboost as xgb
 
 from pysiral.auxdata import AuxdataBaseClass
@@ -71,7 +72,7 @@ class RetrackerThresholdModel(AuxdataBaseClass):
 
         # Verify input
         if len(model_filepath) != 1:
-            msg = f"No or multiple model files found for model_id = {model_id}: {model_filepath}"
+            msg = f"No or multiple model files found for model_id = {model_id}: {self.cfg.local_repository}"
             self.error.add_error("ambigous-input", msg)
             self.error.raise_on_error()
 
@@ -79,26 +80,64 @@ class RetrackerThresholdModel(AuxdataBaseClass):
         self.model_filepath = model_filepath[0]
 
         # NOTE: Danger zone - allowing multi-threading at which level?
+        self.model_input = None
         self.model = xgb.Booster({'nthread': 1})
         self.model.load_model(str(self.model_filepath))
 
     def receive_l1p_input(self, l1p: 'L1bdataNCFile') -> None:
         """
         Optional method to add l1p variables to this class before `get_l2_track_vars()` is
-        called
+        called. This method here grabs the necessary classifier parameter for the model
+        prediction. The parameter names and their data group in the l1p data structure need to
+        be specified in the Level-2 processor definition file as a list of [data_group, parameter_name],
+        e.g.:
+
+            parameter:
+                - ["classifier", "leading_edge_width"]
+                - ["classifier", "sigma0"]
+
+        For this example the parameter list will be reorderd to a feature vector:
+
+            [ [leading_edge_width[0], sigma0[0]], [leading_edge_width[1], sigma0[1]], ... ]
+
         :param l1p:
         :return:
         """
 
-        breakpoint()
+        # Retrieve required parameters for the model prediction
+        parameter_list = self.cfg.options.get("parameter", [])
+        if len(parameter_list) == 0:
+            msg = f"Missing or empty option value parameter: {self.cfg.options}"
+            self.error.add_error("missing-option", msg)
+            self.error.raise_on_error()
+
+        # Retrieve data from l1p data object
+        model_input_list = []
+        for data_group, parameter_name in parameter_list:
+            model_input_list.append(l1p.get_parameter_by_name(data_group, parameter_name))
+
+        # Reform the input data for the model prediction
+        self.model_input = np.array(model_input_list).T
 
     def get_l2_track_vars(self, l2: 'Level2Data') -> None:
         """
-        TODOC:
+        [Mandatory class method] Add the model prediction for the tfmra retracker threshold to the
+        Level-2 data object. The model evaluation depends solely on waveform shape parameters that
+        are not part of the Level-2 data object, because auxiliary data classes are called before the
+        processor steps.
         :param l2:
         :return:
         """
-        breakpoint()
+
+        # Evaluate the model for this case
+        prediction = self.model.predict(xgb.DMatrix(self.model_input))
+
+        # Add prediction to the Level-2 data object
+        var_id, var_name = self.cfg.options.get("output_parameter", ["tfmrathrs_ml", "tfmra_threshold_ml"])
+        l2.set_auxiliary_parameter(var_id, var_name, prediction)
+
+        # Remove the model input (safety measure)
+        self.model_input = None
 
     def get_available_model_files(self, lookup_dir: Union[str, Path], suffixes: Iterable[str]) -> Iterable[Path]:
         """
