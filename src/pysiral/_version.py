7fa1eef86b96cd896d9f0ad9aff74844800eea3b
Stefan Hendricks <stefan.hendricks@awi.de>
2025-09-08 17:10:13 +0200

    surpress stderr output for git request

--- src/pysiral/_version.py
+++ src/pysiral/_version.py
@@ -28,16 +28,36 @@ _THIS_DIR = Path(__file__).parent.resolve()
 
 def get_git_revision_hash(target="HEAD") -> Union[str, None]:
     try:
-        return subprocess.check_output(['git', 'rev-parse', target], cwd=_THIS_DIR).decode('ascii').strip()
+        return subprocess.check_output(
+            ['git', 'rev-parse', target],
+            cwd=_THIS_DIR,
+            stderr=subprocess.PIPE
+        ).decode('ascii').strip()
     except subprocess.SubprocessError:
         return None
 
 
 def get_git_branch() -> Union[str, None]:
     try:
-        return subprocess.check_output(['git', 'branch', '--show-current'], cwd=_THIS_DIR).decode('ascii').strip()
+        return subprocess.check_output(
+            ['git', 'branch', '--show-current'],
+            cwd=_THIS_DIR,
+            stderr=subprocess.PIPE
+        ).decode('ascii').strip()
     except subprocess.CalledProcessError:
         return None
+
+
+def get_git_origin() -> Union[str, None]:
+    try:
+        return subprocess.check_output(
+            ['git', 'remote', 'get-url', 'origin'],
+            cwd=_THIS_DIR,
+            stderr=subprocess.PIPE
+        ).decode('ascii').strip()
+    except subprocess.CalledProcessError:
+        return None
+
 
 try:
     with open(_THIS_DIR / "software-version.toml", mode="rb") as fp:
@@ -54,15 +74,11 @@ except FileNotFoundError:
 
 # Try to get the git information from local git, else default to
 # git-version.toml
-git_version_local = get_git_revision_hash()
-git_branch_local = get_git_branch()
+GIT_VERSION_LOCAL = get_git_revision_hash()
+GIT_BRANCH_LOCAL = get_git_branch()
+GIT_ORIGIN_LOCAL = get_git_origin()
 
 SOFTWARE_VERSION = _software_version_data.get("SOFTWARE_VERSION")
-GIT_VERSION = git_version_local if git_version_local else _git_version_data.get("COMMIT")
-GIT_BRANCH = git_branch_local if git_branch_local else _git_version_data.get("BRANCH")
-GIT_ORIGIN = _git_version_data.get("ORIGIN")
-
-
-
-
-
+GIT_VERSION = GIT_VERSION_LOCAL if GIT_VERSION_LOCAL else _git_version_data.get("COMMIT")
+GIT_BRANCH = GIT_BRANCH_LOCAL if GIT_BRANCH_LOCAL else _git_version_data.get("BRANCH")
+GIT_ORIGIN = GIT_ORIGIN_LOCAL if GIT_ORIGIN_LOCAL else _git_version_data.get("ORIGIN")
